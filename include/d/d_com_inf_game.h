6dc6e321b8ef64d021dd90d7409500ffb7bef4f9
Dylan Ascencio <empressglatia@gmail.com>
2023-09-24 21:54:36 -0400

    Fix conflict

--- include/d/d_com_inf_game.h
+++ include/d/d_com_inf_game.h
@@ -197,8 +197,6 @@ public:
         mItemLifeCount += num;
     }
 
-    u8 getSelectItem(int i_itemIdx) { return mEquippedItems[i_itemIdx]; }
-
     u8 checkMesgCancelButton() { return field_0x4949; }
 
     void setPlayerStatus(int param_0, int i, u32 flag) { mPlayerStatus[param_0][i] |= flag; }
@@ -1117,10 +1115,6 @@ inline s16 dComIfGp_getItemMagicCount() {
 
 inline void dComIfGp_setItemMagicCount(s16 magic) {
     g_dComIfG_gameInfo.play.setItemMagicCount(magic);
-}
-
-inline u8 dComIfGp_getSelectItem(int i_itemIdx) {
-    return g_dComIfG_gameInfo.play.getSelectItem(i_itemIdx);
 }
 
 inline u8 dComIfGp_checkMesgCancelButton() {
