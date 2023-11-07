84ef226b327ca0a32a277ba5b1864ba44187d581
Jasper St. Pierre <jstpierre@mecheye.net>
2023-11-06 18:35:41 -0800

    more JPA work

---
+++ include/JSystem/JParticle/JPADynamicsBlock.h
@@ -0,0 +1,112 @@
+#ifndef JPADYNAMICSBLOCK_H
+#define JPADYNAMICSBLOCK_H
+
+#include "JSystem/JParticle/JPAParticle.h"
+
+struct JPADynamicsBlockData {
+    /* 0x00 */ u32 mFlag;
+    /* 0x04 */ f32 mVolumeSweep;
+    /* 0x08 */ f32 mVolumeMinRad;
+    /* 0x0C */ u16 mVolumeSize;
+    /* 0x0E */ u16 mDivNumber;
+    /* 0x10 */ f32 mRate;
+    /* 0x14 */ f32 mRateRndm;
+    /* 0x18 */ u8 mRateStep;
+    /* 0x19 */ u8 field_0x19;
+    /* 0x1A */ s16 mMaxFrame;
+    /* 0x1C */ s16 mStartFrame;
+    /* 0x1E */ s16 mLifeTime;
+    /* 0x20 */ f32 mLifeTimeRndm;
+    /* 0x24 */ f32 mInitVelOmni;
+    /* 0x28 */ f32 mInitVelAxis;
+    /* 0x2C */ f32 mInitVelRndm;
+    /* 0x30 */ f32 mInitVelDir;
+    /* 0x34 */ f32 mInitVelRatio;
+    /* 0x38 */ f32 mSpread;
+    /* 0x3C */ f32 mAirResist;
+    /* 0x40 */ f32 mAirResistRndm;
+    /* 0x44 */ f32 mMoment;
+    /* 0x48 */ f32 mMomentRndm;
+    /* 0x4C */ f32 mAccel;
+    /* 0x50 */ f32 mAccelRndm;
+    /* 0x54 */ JGeometry::TVec3<f32> mEmitterScl;
+    /* 0x60 */ JGeometry::TVec3<f32> mEmitterTrs;
+    /* 0x6C */ JGeometry::TVec3<f32> mEmitterDir;
+    /* 0x78 */ JGeometry::TVec3<s16> mEmitterRot;
+};
+
+class JPADynamicsBlock {
+public:
+    virtual ~JPADynamicsBlock() {};
+    virtual u32 getDataFlag() = 0;
+    virtual u32 getUseKeyFlag() = 0;
+    virtual void getEmitterScl(JGeometry::TVec3<f32>&) = 0;
+    virtual void getEmitterRot(JGeometry::TVec3<s16>&) = 0;
+    virtual void getEmitterTrs(JGeometry::TVec3<f32>&) = 0;
+    virtual void getEmitterDir(JGeometry::TVec3<f32>&) = 0;
+    virtual u32 getVolumeType() = 0;
+    virtual u32 getVolumeSize() = 0;
+    virtual f32 getVolumeSweep() = 0;
+    virtual f32 getVolumeMinRad() = 0;
+    virtual u32 getDivNumber() = 0;
+    virtual f32 getRate() = 0;
+    virtual f32 getRateRndm() = 0;
+    virtual u32 getRateStep() = 0;
+    virtual u32 getMaxFrame() = 0;
+    virtual u32 getStartFrame() = 0;
+    virtual u32 getLifeTime() = 0;
+    virtual f32 getLifeTimeRndm() = 0;
+    virtual f32 getInitVelOmni() = 0;
+    virtual f32 getInitVelAxis() = 0;
+    virtual f32 getInitVelRndm() = 0;
+    virtual f32 getInitVelDir() = 0;
+    virtual f32 getSpread() = 0;
+    virtual f32 getInitVelRatio() = 0;
+    virtual f32 getAccel() = 0;
+    virtual f32 getAccelRndm() = 0;
+    virtual f32 getAirResist() = 0;
+    virtual f32 getAirResistRndm() = 0;
+    virtual f32 getMoment() = 0;
+    virtual f32 getMomentRndm() = 0;
+};
+
+class JPADynamicsBlockArc : public JPADynamicsBlock {
+public:
+    JPADynamicsBlockArc(const u8*);
+    virtual ~JPADynamicsBlockArc() {}
+    virtual u32 getDataFlag() { return mpData->mFlag; }
+    virtual u32 getUseKeyFlag() { return 0; }
+    virtual void getEmitterScl(JGeometry::TVec3<f32>& out) { out = mpData->mEmitterScl; }
+    virtual void getEmitterRot(JGeometry::TVec3<s16>& out) { out = mpData->mEmitterRot; }
+    virtual void getEmitterTrs(JGeometry::TVec3<f32>& out) { out = mpData->mEmitterTrs; }
+    virtual void getEmitterDir(JGeometry::TVec3<f32>& out) { out = mpData->mEmitterDir; }
+    virtual u32 getVolumeType() { return (mpData->mFlag >> 8) & 0x07; }
+    virtual u32 getVolumeSize() { return mpData->mVolumeSize; }
+    virtual f32 getVolumeSweep() { return mpData->mVolumeSweep; }
+    virtual f32 getVolumeMinRad() { return mpData->mVolumeMinRad; }
+    virtual u32 getDivNumber() { return mpData->mDivNumber; }
+    virtual f32 getRate() { return mpData->mRate; }
+    virtual f32 getRateRndm() { return mpData->mRateRndm; }
+    virtual u32 getRateStep() { return mpData->mRateStep; }
+    virtual u32 getMaxFrame() { return mpData->mMaxFrame; }
+    virtual u32 getStartFrame() { return mpData->mStartFrame; }
+    virtual u32 getLifeTime() { return mpData->mLifeTime; }
+    virtual f32 getLifeTimeRndm() { return mpData->mLifeTimeRndm; }
+    virtual f32 getInitVelOmni() { return mpData->mInitVelOmni; }
+    virtual f32 getInitVelAxis() { return mpData->mInitVelAxis; }
+    virtual f32 getInitVelRndm() { return mpData->mInitVelRndm; }
+    virtual f32 getInitVelDir() { return mpData->mInitVelDir; }
+    virtual f32 getSpread() { return mpData->mSpread; }
+    virtual f32 getInitVelRatio() { return mpData->mInitVelRatio; }
+    virtual f32 getAccel() { return mpData->mAccel; }
+    virtual f32 getAccelRndm() { return mpData->mAccelRndm; }
+    virtual f32 getAirResist() { return mpData->mAirResist; }
+    virtual f32 getAirResistRndm() { return mpData->mAirResistRndm; }
+    virtual f32 getMoment() { return mpData->mMoment; }
+    virtual f32 getMomentRndm() { return mpData->mMomentRndm; }
+
+public:
+    /* 0x04 */ const JPADynamicsBlockData* mpData;
+};
+
+#endif /* JPADynamicsBlock_H */
--- include/JSystem/JParticle/JPAExtraShape.h
+++ include/JSystem/JParticle/JPAExtraShape.h
@@ -3,49 +3,132 @@
 
 #include "JSystem/JParticle/JPAParticle.h"
 
+struct JPAExtraShapeData {
+    /* 0x00 */ u32 mFlag;
+    /* 0x04 */ u32 field_0x04;
+    /* 0x08 */ f32 mAlphaInTiming;
+    /* 0x0C */ f32 mAlphaOutTiming;
+    /* 0x10 */ f32 mAlphaInValue;
+    /* 0x14 */ f32 mAlphaBaseValue;
+    /* 0x18 */ f32 mAlphaOutValue;
+    /* 0x1C */ f32 mAlphaWaveParam1;
+    /* 0x20 */ f32 mAlphaWaveParam2;
+    /* 0x24 */ f32 mAlphaWaveParam3;
+    /* 0x28 */ f32 mAlphaWaveRandom;
+    /* 0x2C */ f32 mScaleInTiming;
+    /* 0x30 */ f32 mScaleOutTiming;
+    /* 0x34 */ f32 mScaleInValueX;
+    /* 0x38 */ f32 mScaleOutValueX;
+    /* 0x3C */ f32 mScaleInValueY;
+    /* 0x40 */ f32 mScaleOutValueY;
+    /* 0x44 */ f32 mRandomScale;
+    /* 0x48 */ s16 mAnmCycleX;
+    /* 0x4A */ s16 mAnmCycleY;
+    /* 0x4C */ f32 mRotateAngle;
+    /* 0x50 */ f32 mRotateSpeed;
+    /* 0x54 */ f32 mRotateRandomAngle;
+    /* 0x58 */ f32 mRotateRandomSpeed;
+    /* 0x5C */ f32 mRotateDirection;
+};
+
 class JPAExtraShape {
 public:
-    virtual ~JPAExtraShape() {};
-    virtual void isEnableScale() = 0;
-    virtual void isDiffXY() = 0;
-    virtual void isEnableScaleAnmX() = 0;
-    virtual void isEnableScaleAnmY() = 0;
-    virtual void isEnableScaleBySpeedX() = 0;
-    virtual void isEnableScaleBySpeedY() = 0;
-    virtual void getScaleInTiming() = 0;
-    virtual void getScaleOutTiming() = 0;
-    virtual void getScaleInValueX() = 0;
-    virtual void getScaleInValueY() = 0;
-    virtual void getAnmTypeX() = 0;
-    virtual void getAnmTypeY() = 0;
-    virtual void getAnmCycleX() = 0;
-    virtual void getAnmCycleY() = 0;
-    virtual void getIncreaseRateX() = 0;
-    virtual void getIncreaseRateY() = 0;
-    virtual void getDecreaseRateX() = 0;
-    virtual void getDecreaseRateY() = 0;
+    virtual ~JPAExtraShape() {}
+    virtual BOOL isEnableScale() = 0;
+    virtual BOOL isDiffXY() = 0;
+    virtual BOOL isEnableScaleAnmX() = 0;
+    virtual BOOL isEnableScaleAnmY() = 0;
+    virtual BOOL isEnableScaleBySpeedX() = 0;
+    virtual BOOL isEnableScaleBySpeedY() = 0;
+    virtual f32 getScaleInTiming() = 0;
+    virtual f32 getScaleOutTiming() = 0;
+    virtual f32 getScaleInValueX() = 0;
+    virtual f32 getScaleInValueY() = 0;
+    virtual u32 getAnmTypeX() = 0;
+    virtual u32 getAnmTypeY() = 0;
+    virtual u32 getAnmCycleX() = 0;
+    virtual u32 getAnmCycleY() = 0;
+    virtual f32 getIncreaseRateX() = 0;
+    virtual f32 getIncreaseRateY() = 0;
+    virtual f32 getDecreaseRateX() = 0;
+    virtual f32 getDecreaseRateY() = 0;
     virtual u32 getPivotX() = 0;
     virtual u32 getPivotY() = 0;
-    virtual void getRandomScale() = 0;
-    virtual void isEnableAlpha() = 0;
-    virtual void isEnableSinWave() = 0;
-    virtual void getAlphaInTiming() = 0;
-    virtual void getAlphaOutTiming() = 0;
-    virtual void getAlphaInValue() = 0;
-    virtual void getAlphaBaseValue() = 0;
-    virtual void getAlphaWaveType() = 0;
-    virtual void getAlphaWaveParam1() = 0;
-    virtual void getAlphaWaveParam2() = 0;
-    virtual void getAlphaWaveParam3() = 0;
-    virtual void getAlphaWaveRandom() = 0;
-    virtual void getAlphaIncreaseRate() = 0;
-    virtual void getAlphaDecreaseRate() = 0;
-    virtual void isEnableRotate() = 0;
-    virtual void getRotateAngle() = 0;
-    virtual void getRotateSpeed() = 0;
-    virtual void getRotateRandomAngle() = 0;
-    virtual void getRotateRandomSpeed() = 0;
-    virtual void getRotateDirection() = 0;
+    virtual f32 getRandomScale() = 0;
+    virtual BOOL isEnableAlpha() = 0;
+    virtual BOOL isEnableSinWave() = 0;
+    virtual f32 getAlphaInTiming() = 0;
+    virtual f32 getAlphaOutTiming() = 0;
+    virtual f32 getAlphaInValue() = 0;
+    virtual f32 getAlphaBaseValue() = 0;
+    virtual u32 getAlphaWaveType() = 0;
+    virtual f32 getAlphaWaveParam1() = 0;
+    virtual f32 getAlphaWaveParam2() = 0;
+    virtual f32 getAlphaWaveParam3() = 0;
+    virtual f32 getAlphaWaveRandom() = 0;
+    virtual f32 getAlphaIncreaseRate() = 0;
+    virtual f32 getAlphaDecreaseRate() = 0;
+    virtual BOOL isEnableRotate() = 0;
+    virtual f32 getRotateAngle() = 0;
+    virtual f32 getRotateSpeed() = 0;
+    virtual f32 getRotateRandomAngle() = 0;
+    virtual f32 getRotateRandomSpeed() = 0;
+    virtual f32 getRotateDirection() = 0;
+};
+
+class JPAExtraShapeArc : public JPAExtraShape {
+public:
+    JPAExtraShapeArc(const u8*);
+    virtual ~JPAExtraShapeArc() {}
+    virtual BOOL isEnableScale() { return (mpData->mFlag & 0x100); }
+    virtual BOOL isDiffXY() { return (mpData->mFlag & 0x200); }
+    virtual BOOL isEnableScaleAnmX() { return (mpData->mFlag & 0x800); }
+    virtual BOOL isEnableScaleAnmY() { return (mpData->mFlag & 0x400); }
+    virtual BOOL isEnableScaleBySpeedX() { return (mpData->mFlag & 0x2000); }
+    virtual BOOL isEnableScaleBySpeedY() { return (mpData->mFlag & 0x1000); }
+    virtual f32 getScaleInTiming() { return mpData->mScaleInTiming; }
+    virtual f32 getScaleOutTiming() { return mpData->mScaleOutTiming; }
+    virtual f32 getScaleInValueX() { return mpData->mScaleInValueX; }
+    virtual f32 getScaleInValueY() { return mpData->mScaleInValueY; }
+    virtual u32 getAnmTypeX() { return (mpData->mFlag >> 18) & 0x01; }
+    virtual u32 getAnmTypeY() { return (mpData->mFlag >> 19) & 0x01; }
+    virtual u32 getAnmCycleX() { return mpData->mAnmCycleX; }
+    virtual u32 getAnmCycleY() { return mpData->mAnmCycleY; }
+    virtual f32 getIncreaseRateX() { return mIncreaseRateX; }
+    virtual f32 getIncreaseRateY() { return mIncreaseRateY; }
+    virtual f32 getDecreaseRateX() { return mDecreaseRateX; }
+    virtual f32 getDecreaseRateY() { return mDecreaseRateY; }
+    virtual u32 getPivotX() { return (mpData->mFlag >> 14) & 0x03; }
+    virtual u32 getPivotY() { return (mpData->mFlag >> 16) & 0x03; }
+    virtual f32 getRandomScale() { return mpData->mRandomScale; }
+    virtual BOOL isEnableAlpha() { return (mpData->mFlag & 0x01); }
+    virtual BOOL isEnableSinWave() { return (mpData->mFlag & 0x02); }
+    virtual f32 getAlphaInTiming() { return mpData->mAlphaInTiming; }
+    virtual f32 getAlphaOutTiming() { return mpData->mAlphaOutTiming; }
+    virtual f32 getAlphaInValue() { return mpData->mAlphaInValue; }
+    virtual f32 getAlphaBaseValue() { return mpData->mAlphaBaseValue; }
+    virtual u32 getAlphaWaveType() { return (mpData->mFlag >> 2) & 0x03; }
+    virtual f32 getAlphaWaveParam1() { return mpData->mAlphaWaveParam1; }
+    virtual f32 getAlphaWaveParam2() { return mpData->mAlphaWaveParam2; }
+    virtual f32 getAlphaWaveParam3() { return mpData->mAlphaWaveParam3; }
+    virtual f32 getAlphaWaveRandom() { return mpData->mAlphaWaveRandom; }
+    virtual f32 getAlphaIncreaseRate() { return mAlphaIncreaseRate; }
+    virtual f32 getAlphaDecreaseRate() { return mAlphaDecreaseRate; }
+    virtual BOOL isEnableRotate() { return (mpData->mFlag & 0x01000000); }
+    virtual f32 getRotateAngle() { return mpData->mRotateAngle; }
+    virtual f32 getRotateSpeed() { return mpData->mRotateSpeed; }
+    virtual f32 getRotateRandomAngle() { return mpData->mRotateRandomAngle; }
+    virtual f32 getRotateRandomSpeed() { return mpData->mRotateRandomSpeed; }
+    virtual f32 getRotateDirection() { return mpData->mRotateDirection; }
+
+public:
+    /* 0x04 */ const JPAExtraShapeData * mpData;
+    /* 0x08 */ f32 mAlphaIncreaseRate;
+    /* 0x0C */ f32 mAlphaDecreaseRate;
+    /* 0x10 */ f32 mIncreaseRateX;
+    /* 0x14 */ f32 mDecreaseRateX;
+    /* 0x18 */ f32 mIncreaseRateY;
+    /* 0x1C */ f32 mDecreaseRateY;
 };
 
 #endif /* JPAEXTRASHAPE_H */
