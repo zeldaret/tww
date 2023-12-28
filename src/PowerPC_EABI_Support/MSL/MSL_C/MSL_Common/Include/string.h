0289ced50845382e253f5ef104bc8560989bc57b
TakaRikka <takarikka@outlook.com>
2023-12-27 21:15:49 -0800

    fix build

--- src/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common/Include/string.h
+++ src/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common/Include/string.h
@@ -9,6 +9,7 @@ extern "C" {
 #endif
 
 __declspec(section ".init") void* memcpy(void* dest, const void* src, size_t n);
+__declspec(section ".init") void __fill_mem(void* dest, int val, size_t count);
 __declspec(section ".init") void* memset(void* dest, int val, size_t count);
 
 int memcmp(const void* lhs, const void* rhs, size_t count);
