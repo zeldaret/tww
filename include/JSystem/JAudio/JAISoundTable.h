656345b354d8c8b7b5ee6c0de5f4609a176b96c7
Jcw87 <Jcw87@users.noreply.github.com>
2024-01-14 00:31:32 -0800

    JAISoundTable

--- include/JSystem/JAudio/JAISoundTable.h
+++ include/JSystem/JAudio/JAISoundTable.h
@@ -6,15 +6,15 @@
 namespace JAInter {
     namespace SoundTable {
         void init(u8*, u32);
-        void getInfoPointer(u32);
-        void getInfoFormat(u32);
+        u8* getInfoPointer(u32);
+        u8 getInfoFormat(u32);
         u8 getCategotyMax();
 
         extern u8 mVersion;
         extern u8 mCategotyMax;
-        extern s16* mSoundMax;
+        extern u16* mSoundMax;
         extern u32 mDatasize;
-        extern int* mPointerCategory;
+        extern u8** mPointerCategory;
         extern u8* mAddress;
     }
 }
