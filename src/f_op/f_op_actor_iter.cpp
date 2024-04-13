b5d054f1528f85c56614529951be1c3ab84d5d2e
Jasper St. Pierre <jstpierre@mecheye.net>
2024-04-13 15:29:15 -0700

    f_op_actor_iter: minor cleanup

--- src/f_op/f_op_actor_iter.cpp
+++ src/f_op/f_op_actor_iter.cpp
@@ -9,23 +9,15 @@
 #include "f_op/f_op_actor_tag.h"
 
 int fopAcIt_Executor(fopAcIt_ExecutorFunc i_execFunc, void* i_data) {
-    struct {
-        fopAcIt_ExecutorFunc func;
-        void* data;
-    } userData;
-    userData.func = i_execFunc;
-    userData.data = i_data;
-
-    return cLsIt_Method(&g_fopAcTg_Queue, (cNdIt_MethodFunc)cTgIt_MethodCall, &userData);
+    method_filter filter;
+    filter.mpMethodFunc = (cNdIt_MethodFunc)i_execFunc;
+    filter.mpUserData = i_data;
+    return cLsIt_Method(&g_fopAcTg_Queue, (cNdIt_MethodFunc)cTgIt_MethodCall, &filter);
 }
 
 void* fopAcIt_Judge(fopAcIt_JudgeFunc i_judgeFunc, void* i_data) {
-    struct {
-        fopAcIt_JudgeFunc func;
-        void* data;
-    } userData;
-    userData.func = i_judgeFunc;
-    userData.data = i_data;
-
-    return cLsIt_Judge(&g_fopAcTg_Queue, (cNdIt_JudgeFunc)cTgIt_JudgeFilter, &userData);
+    judge_filter filter;
+    filter.mpJudgeFunc = (cNdIt_JudgeFunc)i_judgeFunc;
+    filter.mpUserData = i_data;
+    return cLsIt_Judge(&g_fopAcTg_Queue, (cNdIt_JudgeFunc)cTgIt_JudgeFilter, &filter);
 }
