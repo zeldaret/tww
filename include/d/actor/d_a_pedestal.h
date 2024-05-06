7bc933412bc8bbc8e69ef5564257b1302796d469
Daniel Hajjar <33489389+DanTGL@users.noreply.github.com>
2024-05-06 17:57:31 +0200

    d_a_pedestal Work (#623)
    * Started work on d_a_pedestal
    
    * daPedestal::daPds_infiniteEcallBack_c matching
    
    * daPedestal::daPds_c::_delete matching
    
    * d_a_pedestal removed static function declarations
    
    * daPedestal::CheckCreateHeap matching
    
    * daPedestal::daPds_c::getMyStaffId matching
    
    * daPedestal::daPds_c::CreateInit match
    
    * daPedestal::daPds_c::finishCheck Changed return values
    
    * daPedestal::daPds_c::CreateHeap matching
    
    * daPedestal::daPds_c::_create matching
    
    * daPedestal::daPds_c::set_mtx matching
    
    * daPedestal::daPds_c::_execute matching
    
    * daPedestal::daPds_c Started working on events
    
    * daPedestal::daPds_c::setAction matching
    
    * d_a_pedestal WIP
    
    * daPedestal::daPds_infiniteEcallBack_c WIP
    
    * daPedestal::daPds_c::_draw matching
    
    * daPedestal::daPds_c::action matching
    
    * daPedestal::daPds_c::waitAction matching
    
    * daPedestal::daPds_c::eventProc matching
    
    * daPedestal::daPds_c::initialMoveEvent matching
    
    * daPedestal::daPds_c::actionMoveEvent matching
    
    * daPedestal::ChunkCreateHeap Fixed match
    
    * daPedestal::daPds_c::initialDefault matching
    
    * daPedestal::daPds_c::initialEffectSet matching
    
    * daPedestal::daPds_c::initialEffectEnd matching
    
    * daPedestal::daPds_c::playBrkAnm matching
    
    * daPedestal::daPds_c::initBrkAnm matching
    
    * daPedestal::daPds_c Removed auto-generated comments
    
    * d_a_pedestal Added dummy data
    
    * d_a_pedestal Moved event tables to daPedestal
    
    * d_a_pedestal Removed daPds__prm::getParam
    
    * daPedestal::daPds_c Padded end of class
    
    * daPedestal::daPds_c::CreateHeap Fixed conversion matching
    
    * d_a_pedestal Code cleanup
    
    * daPedestal::daPds_infiniteEcallBack_c Inlined setup function
    
    * daPedestal::daPds_infiniteEcallBack_c Added explicit destructor

--- include/d/actor/d_a_pedestal.h
+++ include/d/actor/d_a_pedestal.h
@@ -1,63 +1,87 @@
 #ifndef D_A_PEDESTAL_H
 #define D_A_PEDESTAL_H
 
-#include "f_op/f_op_actor.h"
 #include "d/d_bg_w.h"
+#include "d/d_com_inf_game.h"
+#include "f_op/f_op_actor.h"
+
 #include "d/d_particle.h"
 #include "m_Do/m_Do_ext.h"
 
 namespace daPedestal {
-    static BOOL CheckCreateHeap(fopAc_ac_c*);
-    static s32 daPds_Create(void*);
-    static BOOL daPds_Delete(void*);
-    static BOOL daPds_Draw(void*);
-    static BOOL daPds_Execute(void*);
-    static BOOL daPds_IsDelete(void*);
-
-    class daPds_c : public fopAc_ac_c {
-    public:
-        BOOL _delete();
-        void CreateHeap();
-        void CreateInit();
-        s32 _create();
-        void getMyStaffId();
-        void wakeupCheck();
-        void finishCheck();
-        void setAction(int (daPedestal::daPds_c::*)(void*), void*);
-        void action(void*);
-        void waitAction(void*);
-        void eventProc();
-        void initialDefault(int);
-        void actionDefault(int);
-        void initialMoveEvent(int);
-        void actionMoveEvent(int);
-        void initialEffectSet(int);
-        void initialEffectEnd(int);
-        void set_mtx();
-        void initBrkAnm(unsigned char, bool);
-        void playBrkAnm();
-        BOOL _execute();
-        BOOL _draw();
-    
-    public:
-        /* 0x290 */ u8 m290[0x298 - 0x290];
-        /* 0x298 */ J3DModel* mpModel;
-        /* 0x29C */ mDoExt_brkAnm mBrk;
-        /* 0x2B4 */ u8 m2B4[0x2E4 - 0x2B4];
-        /* 0x2E4 */ dBgW* mpBgW;
-        /* 0x2E8 */ u8 m2E8[0x324 - 0x2E8];
-    };
 
     class daPds_infiniteEcallBack_c : public dPa_levelEcallBack {
+    public:
+        virtual ~daPds_infiniteEcallBack_c() {}
+
         void execute(JPABaseEmitter*);
         void end();
         void makeEmitter(unsigned short, const cXyz*, const csXyz*, const cXyz*);
 
-        void getEmitter() {}
-        void setAngle(const csXyz*) {}
-        void setPos(const cXyz*) {}
-        void setup(JPABaseEmitter*, const cXyz*, const csXyz*, signed char) {}
+        JPABaseEmitter* getEmitter() { return mpEmitter; }
+        void setAngle(const csXyz* angle) { mpAngle = angle; }
+        void setPos(const cXyz* pos) { mpPos = pos; }
+        void setup(JPABaseEmitter* emitter, const cXyz*, const csXyz*, signed char) {
+            mpEmitter = emitter;
+        }
+
+    private:
+        /* 0x04 */ JPABaseEmitter* mpEmitter;
+        /* 0x08 */ const cXyz* mpPos;
+        /* 0x08 */ const csXyz* mpAngle;
     };
-};
+
+    class daPds_c : public fopAc_ac_c {
+    public:
+        typedef int (daPds_c::* ActionFunc_t)(void*);
+
+        BOOL _delete();
+        BOOL CreateHeap();
+        void CreateInit();
+        s32 _create();
+        int getMyStaffId();
+        BOOL wakeupCheck();
+        BOOL finishCheck();
+        BOOL setAction(ActionFunc_t, void*);
+        void action(void*);
+        BOOL waitAction(void*);
+        BOOL eventProc();
+        void initialDefault(int);
+        BOOL actionDefault(int);
+        void initialMoveEvent(int);
+        BOOL actionMoveEvent(int);
+        void initialEffectSet(int);
+        void initialEffectEnd(int);
+        void set_mtx();
+        BOOL initBrkAnm(u8, bool);
+        void playBrkAnm();
+        BOOL _execute();
+        BOOL _draw();
+
+        static const char m_arcname[];
+
+    public:
+        /* 0x290 */ request_of_phase_process_class mPhs;
+        /* 0x298 */ J3DModel* mpModel;
+        /* 0x29C */ mDoExt_brkAnm mBrk;
+        /* 0x2B4 */ Mtx mMtx;
+        /* 0x2E4 */ dBgW* mpBgW;
+        /* 0x2E8 */ daPds_infiniteEcallBack_c mInfiniteEcallBack;
+        /* 0x2F8 */ ActionFunc_t mAction;
+        /* 0x304 */ f32 unk304;
+        /* 0x308 */ f32 unk308;
+        /* 0x30C */ s8 unk30C;
+        /* 0x30D */ u8 mParam;
+        /* 0x30E */ u8 unk30E;
+        /* 0x30F */ s8 unk30F;
+        /* 0x310 */ s16 unk310;
+        /* 0x312 */ s16 unk312;
+        /* 0x314 */ s16 unk314;
+        /* 0x316 */ s16 unk316;
+        /* 0x318 */ u8 pad318[4];
+        /* 0x31C */ f32 unk31C;
+        /* 0x320 */ u8 pad320[4];
+    };
+};  // namespace daPedestal
 
 #endif /* D_A_PEDESTAL_H */
