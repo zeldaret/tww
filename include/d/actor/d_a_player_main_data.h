9ac4cca1ae5c2fc93092a16cd3ad270d669b949b
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2024-05-18 22:57:05 -0400

    Add Back Static

--- include/d/actor/d_a_player_main_data.h
+++ include/d/actor/d_a_player_main_data.h
@@ -38,10 +38,9 @@ extern const Vec l_crawl_lside_front_offset;
 extern const Vec l_crawl_rside_front_offset;
 extern const Vec l_crawl_min_side_offset;
 
-// I believe these won't link properly with the static variables
-//extern dCcD_SrcCyl l_cyl_src;
+extern dCcD_SrcCyl l_cyl_src;
 extern dCcD_SrcCyl l_wind_cyl_src;
 extern dCcD_SrcCyl l_at_cyl_src;
-//extern dCcD_SrcCps l_at_cps_src;
+extern dCcD_SrcCps l_at_cps_src;
 extern dCcD_SrcCps l_fan_wind_cps_src;
 extern dCcD_SrcSph l_fan_wind_sph_src;
