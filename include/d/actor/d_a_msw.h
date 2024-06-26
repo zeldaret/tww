9414f574a6db633350ce529b5634b3efc45d019e
DanTGL <33489389+DanTGL@users.noreply.github.com>
2024-06-26 21:59:49 +0200

    d_a_msw Removed padding

--- include/d/actor/d_a_msw.h
+++ include/d/actor/d_a_msw.h
@@ -13,12 +13,10 @@ public:
     /* 0x29A */ s16 m29A;
     /* 0x29C */ J3DModel* mpModel;
     /* 0x2A0 */ u8 m2A0;
-    /* 0x2A1 */ u8 m2A1[0x2A4 - 0x2A1];
     /* 0x2A4 */ cXyz m2A4;
     /* 0x2B0 */ cXyz m2B0;
     /* 0x2BC */ cXyz m2BC;
     /* 0x2C8 */ csXyz m2C8;
-    /* 0x2CE */ u8 m2CE[0x2D0 - 0x2CE];
     /* 0x2D0 */ J3DModel* mpChainModels[4];
     /* 0x2E0 */ cXyz m2E0[4];
     /* 0x310 */ cXyz m310[4];
@@ -26,7 +24,7 @@ public:
     /* 0x37C */ dCcD_Cyl mChainCyls[4];
     /* 0x83C */ s16 m83C[4];
     /* 0x844 */ s16 m844;
-    /* 0x846 */ u8 m846[0x84C - 0x846];
+    /* 0x846 */ u8 m846[0x84C - 0x846]; // ?
     /* 0x84C */ Mtx mMtx;
     /* 0x87C */ dBgW* mpBgW;
 };
