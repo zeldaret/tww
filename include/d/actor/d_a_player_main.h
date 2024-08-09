8566da624775633fc83eb62c30fe364398cfc1c1
DanTGL <33489389+DanTGL@users.noreply.github.com>
2024-08-09 18:44:03 +0200

    Removed another old comment

--- include/d/actor/d_a_player_main.h
+++ include/d/actor/d_a_player_main.h
@@ -275,7 +275,7 @@ public:
     /* 0x018 */ int field_0x018;
     /* 0x01C */ int mBlurColorType;
     /* 0x020 */ f32 mBlurTopRate;
-    /* 0x024 */ Vec* mpPosBuffer; // Pointer to tuple array? (could possibly use typedef for readability)
+    /* 0x024 */ Vec* mpPosBuffer;
     /* 0x028 */ cXyz field_0x028;
     /* 0x034 */ cXyz field_0x034[60];
     /* 0x304 */ cXyz field_0x304[60];
