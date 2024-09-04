284c0ad5790ded17fcd4ee515979550a1b420a8c
DanTGL <33489389+DanTGL@users.noreply.github.com>
2024-09-04 14:53:46 +0200

    d_a_pirate_flag started work

--- include/d/actor/d_a_pirate_flag.h
+++ include/d/actor/d_a_pirate_flag.h
@@ -2,8 +2,12 @@
 #define D_A_PIRATE_FLAG_H
 
 #include "f_op/f_op_actor.h"
+#include "d/d_com_inf_game.h"
+#include "m_Do/m_Do_hostIO.h"
+#include "JSystem/J3DGraphBase/J3DPacket.h"
 
-class daPirate_Flag_packet_c {
+
+class daPirate_Flag_packet_c : public J3DPacket {
 public:
     void changeCurrentPos() {}
     void getEscapeNrmAngle() {}
@@ -14,15 +18,28 @@ public:
     void setNrmMtx() {}
     void setTevStr(dKy_tevstr_c*) {}
 
-    void setCorrectNrmAngle(short, float);
+    void setCorrectNrmAngle(s16, f32);
     void setBackNrm();
     void setNrmVtx(cXyz*, int, int);
     void draw();
 };
 
+class daPirate_Flag_HIO_c : public JORReflexible {
+public:
+    daPirate_Flag_HIO_c() {}
+    virtual ~daPirate_Flag_HIO_c() {
+        mChildID = -1;
+    }
+
+public:
+    /* 0x04 */ s8 mChildID;
+};
+
 class pirate_flag_class : public fopAc_ac_c {
 public:
     /* Place member variables here */
+    /* 0x290 */ request_of_phase_process_class mPhs1;
+    /* 0x298 */ request_of_phase_process_class mPhs2;
 };
 
 #endif /* D_A_PIRATE_FLAG_H */
