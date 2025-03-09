bad27f4e573af92253e720a89ce207f4fb5cac44
Ross Newman <rzn3102@gmail.com>
2025-03-08 21:22:50 -0500

    start d-a-obj-swhammer

--- include/d/actor/d_a_obj_swhammer.h
+++ include/d/actor/d_a_obj_swhammer.h
@@ -1,22 +1,26 @@
 #ifndef D_A_OBJ_SWHAMMER_H
 #define D_A_OBJ_SWHAMMER_H
 
-#include "f_op/f_op_actor.h"
+#include "d/d_a_obj.h"
+#include "d/d_bg_s_movebg_actor.h"
+#include "c/c_damagereaction.h"
 
 class J3DNode;
 
 namespace daObjSwhammer {
-    class Act_c : public fopAc_ac_c {
+    class Act_c : public dBgS_MoveBgActor {
     public:
-        void is_switch() const {}
+        BOOL is_switch() const {
+            return fopAcM_isSwitch(const_cast<Act_c*>(this), prm_get_swSave());
+        }
         void off_switch2() const {}
         void on_switch() const {}
-        void prm_get_swSave() const {}
+        s32 prm_get_swSave() const { return -1; }
         void prm_get_swSave2() const {}
     
         Act_c();
-        void CreateHeap();
-        s32 Create();
+        int CreateHeap();
+        int Create();
         s32 _create();
         BOOL Delete();
         bool _delete();
@@ -38,12 +42,44 @@ namespace daObjSwhammer {
         void mode_lower();
         void mode_l_u_init();
         void mode_l_u();
-        void Execute(float(**)[3][4]);
+        BOOL Execute(Mtx**);
         BOOL Draw();
+
+        static int M_damage;
+        static short M_damage_dir;
+        static Mtx M_tmp_mtx;
     
+        static const char M_arcname[7];
+        static const dCcD_SrcCyl M_cyl_src_co;
+        static const dCcD_SrcCyl M_cyl_src_tg;
+
     public:
-        /* Place member variables here */
-    };
+        /* 0x2C8 */ request_of_phase_process_class mPhs;
+        /* 0x2D0 */ J3DModel* mpModel;
+        /* 0x2D4 */ dCcD_Cyl mCylCo;
+        /* 0x404 */ dCcD_Stts mSttsCo;
+        /* 0x440 */ dCcD_Cyl mCylTg;
+        /* 0x570 */ dCcD_Stts mSttsTg;
+        /* 0x5AC */ int mMode;
+        /* 0x5B0 */ short m5B0;
+        /* 0x5B2 */ u8 m5B2;
+        /* 0x5B4 */ float m5B4;
+        /* 0x5B8 */ float m5B8;
+        /* 0x5BC */ float m5BC;
+        /* 0x5C0 */ float m5C0;
+        /* 0x5C4 */ float m5C4;
+        /* 0x5C8 */ float m5C8;
+        /* 0x5CC */ float m5CC;
+        /* 0x5D0 */ float m5D0;
+        /* 0x5D4 */ float m5D4;
+        /* 0x5D8 */ float mTopPos;
+        /* 0x5DC */ dPa_followEcallBack m5DC; // TODO verify
+        /* 0x5F0 */ u8 m5F0[0x5FC - 0x5F0];
+        /* 0x5FC */ bool m5FC;
+
+    }; /* Size: 0x600 */
+
+    // TODO STATIC_ASSERT(sizeof(Act_c) == 0x600);
 };
 
 #endif /* D_A_OBJ_SWHAMMER_H */
