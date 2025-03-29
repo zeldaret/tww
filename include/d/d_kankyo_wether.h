b03e563738ea981034fd620a4e9290ceabcb20c5
‎ ‎ ‎ <  >
2025-03-29 18:24:54 -0400

    d_a_player_dproc.inc - All except two fully matching.

--- include/d/d_kankyo_wether.h
+++ include/d/d_kankyo_wether.h
@@ -369,6 +369,7 @@ void dKyw_evt_wind_set(s16 i_windX, s16 i_windY);
 void dKyw_evt_wind_set_go();
 void dKyw_get_AllWind_vec(cXyz* param_0, cXyz* i_direction, f32* i_power);
 cXyz dKyw_get_AllWind_vecpow(cXyz* param_0);
+int dKyw_get_tactwind_dir();
 BOOL dKyw_gbwind_use_check();
 void dKyw_tornado_Notice(cXyz* param_0);
 
