d1cf5aa2bbf52d4808cf3b1c283d9cfe7556b02b
Jcw87 <Jcw87@users.noreply.github.com>
2025-05-08 09:05:39 -0700

    make std::__tag_va_List work with other compilers

--- include/JSystem/JUtility/JUTDirectPrint.h
+++ include/JSystem/JUtility/JUTDirectPrint.h
@@ -6,7 +6,9 @@
 
 namespace std {
     // no idea what this is or why it is used instead of va_list
-    struct __tag_va_List : __va_list_struct {};
+    struct __tag_va_List {
+        va_list list;
+    };
 }
 
 class JUTDirectPrint {
