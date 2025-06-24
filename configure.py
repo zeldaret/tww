5f78fe0d29148d2bcc95b4f73cb118ce8f0661ee
LagoLunatic <LagoLunatic@users.noreply.github.com>
2025-06-23 22:09:57 -0400

    Update dtk-template

--- configure.py
+++ configure.py
@@ -106,6 +106,12 @@ parser.add_argument(
     help="path to sjiswrap.exe (optional)",
 )
 parser.add_argument(
+    "--ninja",
+    metavar="BINARY",
+    type=Path,
+    help="path to ninja binary (optional)"
+)
+parser.add_argument(
     "--verbose",
     action="store_true",
     help="print verbose output",
@@ -143,6 +149,7 @@ config.compilers_path = args.compilers
 config.generate_map = args.map
 config.non_matching = args.non_matching
 config.sjiswrap_path = args.sjiswrap
+config.ninja_path = args.ninja
 config.progress = args.progress
 if not is_windows():
     config.wrapper = args.wrapper
@@ -152,8 +159,8 @@ if args.no_asm:
 # Tool versions
 config.binutils_tag = "2.42-1"
 config.compilers_tag = "20250520"
-config.dtk_tag = "v1.5.1"
-config.objdiff_tag = "v3.0.0-beta.8"
+config.dtk_tag = "v1.6.2"
+config.objdiff_tag = "v3.0.0-beta.10"
 config.sjiswrap_tag = "v1.2.1"
 config.wibo_tag = "0.6.16"
 
