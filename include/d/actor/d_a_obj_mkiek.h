10ab2674b32edd3a44482f0a3a08b138fb1c3034
Mikhail Polovnev <lih.meh@gmail.com>
2025-07-13 17:32:55 +0300

    match d_a_obj_mkiek - main batch

--- include/d/actor/d_a_obj_mkiek.h
+++ include/d/actor/d_a_obj_mkiek.h
@@ -2,12 +2,13 @@
 #define D_A_OBJ_MKIEK_H
 
 #include "d/d_bg_s_movebg_actor.h"
+#include "m_Do/m_Do_ext.h"
 
 namespace daObjMkiek {
     class Act_c : public dBgS_MoveBgActor {
     public:
-        void prm_get_sound() const {}
-        void prm_get_swSave() const {}
+        int prm_get_sound() const;
+        int prm_get_swSave() const;
     
         virtual BOOL CreateHeap();
         virtual BOOL Create();
@@ -21,9 +22,33 @@ namespace daObjMkiek {
         void demo();
         virtual BOOL Execute(Mtx**);
         virtual BOOL Draw();
+
+        enum Prm_e {
+            PRM_SWSAVE_W = 0x08,
+            PRM_SWSAVE_S = 0x00,
+
+            PRM_SOUND_W = 0x01,
+            PRM_SOUND_S = 0x08
+        };
+
+        static const char M_arcname[];
+        static const char M_envname[];
+
+    private:
+        static Mtx M_tmp_mtx;
     
     public:
-        /* Place member variables here */
+        /* 0x2C8 */ request_of_phase_process_class mPhs;
+        /* 0x2D0 */ J3DModel* mpModel;
+        /* 0x2D4 */ J3DModel* mpModelV;
+        /* 0x2D8 */ mDoExt_brkAnm mBrkAnm;
+        /* 0x2F0 */ dCcD_Stts mStts;
+        /* 0x32C */ dCcD_Sph mSph;
+        /* 0x458 */ bool m458;
+        /* 0x459 */ u8 unmatched1; // TODO: match
+        /* 0x45A */ s16 mDieEventIdx;
+        /* 0x45C */ s32 m45C;
+        /* 0x460 */ s32 m460;
     };
 };
 
