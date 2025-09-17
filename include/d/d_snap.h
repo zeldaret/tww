6980cbaa21d5f276543abfaf43bf19fb0b1d8d09
maimai0254 <90623241+maimai0254@users.noreply.github.com>
2025-09-17 23:56:12 +0900

    d_a_bdk (#888)
    * d_a_bdk - 98%
    
    * C-style
    
    * maybe 100%
    
    * Resolve protected access violation
    
    * Set configure.py to matching

--- include/d/d_snap.h
+++ include/d/d_snap.h
@@ -207,7 +207,7 @@ enum {
     /* 0xC7 */ DSNAP_TYPE_BTD,
     /* 0xC8 */ DSNAP_TYPE_BMD,
     /* 0xC9 */ DSNAP_TYPE_BST,
-    /* 0xCA */ DSNAP_TYPE_UNKCA,
+    /* 0xCA */ DSNAP_TYPE_BDK,
     /* 0xCB */ DSNAP_TYPE_UNKCB,
     /* 0xCC */ DSNAP_TYPE_BWD,
     /* 0xCD */ DSNAP_TYPE_UNKCD,
