30b1b09d5ce3ccb0f5a96d972f38d61ac52bd9f8
Zeclos <Zeclos99@gmail.com>
2025-12-31 17:09:46 +0100

    even more cleanup

--- include/d/actor/d_a_fm.h
+++ include/d/actor/d_a_fm.h
@@ -10,14 +10,6 @@ public:
         PROC_0_e,
         PROC_1_e,
     };
-
-    enum Prm_e {
-        PRM_SWSAVE_W = 0x04, // unsure about names
-        PRM_SWSAVE_S = 0x18,
-
-        PRM_SOUND_W = 0x01,
-        PRM_SOUND_S = 0x08
-    };
     
     bool isBodyAppear() {return mMode != 1 && mMode != 0x12 && mMode != 0 && (mMode != 2 && (mMode != 4) && (mMode != 3 && (mMode != 0x11)));}
     bool isHoleAppear() {return field_0x3E0 > 0.015f;} // unsure?
@@ -140,7 +132,6 @@ public:
     /* 0x2C6 */ s8 mOldAnmPrmIdx;
     /* 0x2C7 */ u8 field_0x2C7;
     /* 0x2C8 */ s16 field_0x2C8;
-    /* 0x2CA */ u8 field_0x2CA[0x2CC - 0x2CA];
     /* 0x2CC */ int m_path_no;
     /* 0x2D0 */ int field_0x2D0;
     /* 0x2D4 */ int field_0x2D4;
@@ -158,7 +149,6 @@ public:
     /* 0x3A4 */ cXyz field_0x3A4;
     /* 0x3B0 */ cXyz field_0x3B0;
     /* 0x3BC */ s8 field_0x3BC;
-    /* 0x3BD */ u8 field_0x3BD[0x3C0 - 0x3BD];
     /* 0x3C0 */ dPath* mpPath;
     /* 0x3C4 */ u8 field_0x3C4[0x3C8 - 0x3C4];
     /* 0x3C8 */ J3DModel* mpModel;
@@ -168,7 +158,6 @@ public:
     /* 0x3F0 */ request_of_phase_process_class mPhs;
     /* 0x3F8 */ u8 field_0x3F8[0x3FC - 0x3F8];
     /* 0x3FC */ bool field_0x3FC;
-    /* 0x3FD */ u8 field_0x3FD[0x400 - 0x3FD];
     /* 0x400 */ mDoExt_McaMorf* mpMorf;
     /* 0x404 */ dBgS_ObjAcch mObjAcch;
     /* 0x5C8 */ dBgS_AcchCir mAcchCir;
@@ -189,20 +178,16 @@ public:
     /* 0x670 */ fpc_ProcID mProcId;
     /* 0x674 */ cXyz mGrabPos;
     /* 0x680 */ s16 field_0x680;
-    /* 0x682 */ u8 field_0x682[0x684 - 0x682];
     /* 0x684 */ int field_0x684;
     /* 0x688 */ bool field_0x688;
-    /* 0x689 */ u8 field_0x689[0x68A - 0x689];
     /* 0x68A */ s16 field_0x68A;
     /* 0x68C */ s16 field_0x68C;
-    /* 0x68E */ u8 field_0x68E[0x690 - 0x68E];
     /* 0x690 */ cXyz field_0x690;
     /* 0x69C */ cXyz field_0x69C;
     /* 0x6A8 */ fpc_ProcID mProcId2;
     /* 0x6AC */ u8 field_0x6AC[0x6B4 - 0x6AC];
     /* 0x6B4 */ int field_0x6B4;
     /* 0x6B8 */ u8 field_0x6B8;
-    /* 0x6B9 */ u8 field_0x6B9[0x6BC - 0x6B9];
     /* 0x6BC */ Mtx field_0x6BC;
     /* 0x6EC */ JntHit_c* mpJntHit;
     /* 0x6F0 */ dCcD_Stts mStts;
@@ -211,10 +196,8 @@ public:
     /* 0x894 */ dCcD_Cyl mCyl;
     /* 0x9C4 */ int field_0x9C4;
     /* 0x9C8 */ u8 mHitType;
-    /* 0x9C9 */ u8 field_0x9C9[0x9CC - 0x9C9];
     /* 0x9CC */ fopAc_ac_c* mBaseTarget;
     /* 0x9D0 */ s16 field_0x9D0;
-    /* 0x9D2 */ u8 field_0x9D2[0x9D4 - 0x9D2];
     /* 0x9D4 */ f32 field_0x9D4;
     /* 0x9D8 */ dBgS_ObjLinChk mLinChk;
     /* 0xA44 */ u8 field_0xA44[0xA48 - 0xA44];
@@ -222,7 +205,6 @@ public:
     /* 0xAD8 */ bool field_0xAD8[12];
     /* 0xAE4 */ u8 field_0xAE4;
     /* 0xAE5 */ u8 field_0xAE5;
-    /* 0xAE6 */ u8 field_0xAE6[0xAE8 - 0xAE6];
     /* 0xAE8 */ enemyice mEnemyIce;
 };  // Size: 0xEA0
 
