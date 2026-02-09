b5ddc2ed9f0a9068383b3946b94b7c7bf16d6dca
Briggs Baltzell <briggsbaltzellb3@gmail.com>
2026-02-09 00:26:30 -0600

    Implement `draw` methods for `dDlst_MENU_CAPTURE_c` and `dDlst_MENU_CLOTH_c` in `d_menu_window`.

--- src/d/d_menu_window.cpp
+++ src/d/d_menu_window.cpp
@@ -13,6 +13,7 @@
 #include "d/d_priority.h"
 #include "d/d_procname.h"
 #include "m_Do/m_Do_controller_pad.h"
+#include "m_Do/m_Do_graphic.h"
 
 // TODO: Remove me when all the JUT asserts and other strings are filled out.
 // This is temporary, just to make the strings match.
@@ -52,14 +53,92 @@ void dummy0() {
     OSReport("rfonttype != 0");
 }
 
-class dDlst_MENU_CLOTH_c : public dDlst_base_c {};
+class dDlst_MENU_CLOTH_c : public dDlst_base_c {
+public:
+    virtual void draw();
+};
 
 class dDlst_MENU_CAPTURE_c : public dDlst_base_c {
 public:
     dDlst_MENU_CAPTURE_c() { mStatus = 0; }
 
+    virtual void draw();
+
     /* 0x04 */ u8 mStatus;
 };
+
+void dDlst_MENU_CAPTURE_c::draw() {
+    if (mStatus == 1) {
+        mStatus = 2;
+        GXSetTexCopySrc(0, 0, 0x280, 0x1e0);
+        GXSetTexCopyDst(0x140, 0xf0, GXTexFmt(mDoGph_gInf_c::mFrameBufferTimg->format), 1);
+        GXCopyTex(mDoGph_gInf_c::mFrameBufferTex, 0);
+        GXPixModeSync();
+    } else {
+        GXSetTexCopySrc(0, 0, 0x280, 0x1e0);
+        GXSetTexCopyDst(0x140, 0xf0, GX_TF_RGB565, 1);
+        GXCopyTex(mDoGph_gInf_c::mZbufferTex, 0);
+        GXPixModeSync();
+        GXTexObj tex_obj;
+        GXInitTexObj(&tex_obj, mDoGph_gInf_c::mFrameBufferTex, 0x140, 0xf0, GXTexFmt(mDoGph_gInf_c::mFrameBufferTimg->format), GX_CLAMP, GX_CLAMP, false);
+        GXInitTexObjLOD(&tex_obj, GX_LINEAR, GX_LINEAR, 0.0, 0.0, 0.0, 0, 0, GX_ANISO_1);
+        GXLoadTexObj(&tex_obj, GX_TEXMAP0);
+        GXSetNumChans(0);
+        GXSetNumTexGens(1);
+        GXSetTexCoordGen2(GX_TEXCOORD0, GX_TG_MTX2x4, GX_TG_TEX0, GX_IDENTITY, false, GX_PTIDENTITY);
+        GXSetNumTevStages(1);
+        GXSetTevOrder(GX_TEVSTAGE0, GX_TEXCOORD0, GX_TEXMAP0, GX_COLOR_NULL);
+        GXSetTevColorIn(GX_TEVSTAGE0, GX_CC_ZERO, GX_CC_ZERO, GX_CC_ZERO, GX_CC_TEXC);
+        GXSetTevColorOp(GX_TEVSTAGE0, GX_TEV_ADD, GX_TB_ZERO, GX_CS_SCALE_1, true, GX_TEVPREV);
+        GXSetTevAlphaIn(GX_TEVSTAGE0, GX_CA_ZERO, GX_CA_ZERO, GX_CA_ZERO, GX_CA_ZERO);
+        GXSetTevAlphaOp(GX_TEVSTAGE0, GX_TEV_ADD, GX_TB_ZERO, GX_CS_SCALE_1, true, GX_TEVPREV);
+        GXSetZCompLoc(1);
+        GXSetZMode(false, GX_ALWAYS, false);
+        GXSetBlendMode(GX_BM_NONE, GX_BL_ZERO, GX_BL_ZERO, GX_LO_OR);
+        GXSetAlphaCompare(GX_ALWAYS, 0, GX_AOP_OR, GX_ALWAYS, 0);
+        GXColor local_68 = g_clearColor;
+        GXSetFog(GX_FOG_NONE, 0.0, 0.0, 0.0, 0.0, local_68);
+        GXSetFogRangeAdj(false, 0, NULL);
+        GXSetCullMode(GX_CULL_NONE);
+        GXSetDither(1);
+        Mtx44 mtx;
+        C_MTXOrtho(mtx, 0.0, 1.0, 0.0, 1.0, 0.0, 10.0);
+        GXSetProjection(mtx, GX_ORTHOGRAPHIC);
+        GXLoadPosMtxImm(mDoMtx_getIdentity(), 0);
+        GXSetCurrentMtx(0);
+        GXClearVtxDesc();
+        GXSetVtxDesc(GX_VA_POS, GX_DIRECT);
+        GXSetVtxDesc(GX_VA_TEX0, GX_DIRECT);
+        GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_CLR_RGBA, GX_RGB8, 0);
+        GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_CLR_RGBA, GX_RGB8, 0);
+        GXBegin(GX_QUADS, GX_VTXFMT0, 4);
+
+        GXPosition1x8(0);
+        GXPosition1x8(0);
+        GXFIFO.s8 = -5;
+        GXPosition1x8(0);
+        GXPosition1x8(0);
+        GXPosition1x8(1);
+        GXPosition1x8(0);
+        GXFIFO.s8 = -5;
+        GXPosition1x8(1);
+        GXPosition1x8(0);
+        GXPosition1x8(1);
+        GXPosition1x8(1);
+        GXFIFO.s8 = -5;
+        GXPosition1x8(1);
+        GXPosition1x8(1);
+        GXPosition1x8(0);
+        GXPosition1x8(1);
+        GXFIFO.s8 = -5;
+        GXPosition1x8(0);
+        GXPosition1x8(1);
+    }
+}
+
+void dDlst_MENU_CLOTH_c::draw() {
+    // TODO
+}
 
 static dMCloth_c* cloth_c;
 static dDlst_MENU_CLOTH_c* dMs_cloth_c;
