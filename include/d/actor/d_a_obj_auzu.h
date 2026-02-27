c7a39a036b6f295b1ddc8140b2d00e097c344511
Roland <roland@wildpaws.xyz>
2026-02-27 01:26:11 -0600

    59% matching in .text

--- include/d/actor/d_a_obj_auzu.h
+++ include/d/actor/d_a_obj_auzu.h
@@ -1,40 +1,61 @@
 #ifndef D_A_OBJ_AUZU_H
 #define D_A_OBJ_AUZU_H
 
+#include "d/d_a_obj.h"
 #include "f_op/f_op_actor.h"
+#include "m_Do/m_Do_ext.h"
 
 class J3DMaterial;
 
 namespace daObjAuzu {
+    struct Attr_c {
+        f32 m00;
+        f32 m04;
+        f32 m08;
+        f32 m0C;
+    };
+    
     class Act_c : public fopAc_ac_c {
     public:
-        void attr() const {}
-        void is_appear() const {}
+
+    public:
+        const Attr_c& attr() const { return M_attr[field_0x2B0]; }
+        bool is_appear() const { return field_0x2B4 > 0.999f; }
         void prm_get_appear() const {}
         void prm_get_linkID() const {}
-        void prm_get_swSave() const {}
-        void prm_get_type() const {}
+        s32 prm_get_swSave() const { return daObj::PrmAbstract(this, 0x08, 0x00); }
+        s32 prm_get_type() const { return daObj::PrmAbstract(this, 0x04, 0x14); }
         void prm_make_squid() {}
-        void to_appear() {}
-        void to_disappear() {}
+        void to_appear() { field_0x2B8 = true; }
+        void to_disappear() { field_0x2B8 = false; }
     
-        void solidHeapCB(fopAc_ac_c*);
-        void create_heap();
+        bool create_heap();
         cPhs_State _create();
         bool _delete();
-        void is_exist() const;
+        bool is_exist() const;
         void set_mtx();
         void init_mtx();
         void set_state_map();
         void ship_whirl();
         void bgm_start();
         bool _execute();
-        void set_material(J3DMaterial*, unsigned char);
         bool _draw();
-    
+        static BOOL solidHeapCB(fopAc_ac_c*);            
+        static void set_material(J3DMaterial*, unsigned char);
+        static const char M_arcname[];
+        static const Attr_c M_attr[2];
     public:
-        /* Place member variables here */
-    };
+        /* 0x290 */ request_of_phase_process_class field_0x290;
+        /* 0x298 */ J3DModel* field_0x298;
+        /* 0x29C */ mDoExt_btkAnm field_0x29C;
+        /* 0x2B0 */ s32 field_0x2B0;
+        /* 0x2B4 */ f32 field_0x2B4;
+        /* 0x2B8 */ u8 field_0x2B8;
+        /* 0x2B9 */ u8 field_0x2B9;
+        /* 0x2BA */ u8 field_0x2BA;
+        /* 0x2BB */ u8 field_0x2BB[0x2BC - 0x2BB];
+        /* 0x2BC */ s32 field_0x2BC;
+    };  // Size: 0x2C0
 };
 
 #endif /* D_A_OBJ_AUZU_H */
--- src/d/actor/d_a_obj_auzu.cpp
+++ src/d/actor/d_a_obj_auzu.cpp
@@ -8,44 +8,135 @@
 #include "d/d_procname.h"
 #include "d/d_priority.h"
 
+namespace daObjAuzu {
+namespace {
+f32 L_radius = 2500.0f;
+};
+
+const char Act_c::M_arcname[] = "Auzu";
+const Attr_c Act_c::M_attr[2] = {
+    { 1.5f, 1.5f, 1.3f, 0.5f },
+    { 2.0f, 2.0f, 2.0f, 1.0f }
+};
+};
+
 /* 00000078-0000009C       .text solidHeapCB__Q29daObjAuzu5Act_cFP10fopAc_ac_c */
-void daObjAuzu::Act_c::solidHeapCB(fopAc_ac_c*) {
-    /* Nonmatching */
+BOOL daObjAuzu::Act_c::solidHeapCB(fopAc_ac_c* i_this) {
+    return ((Act_c*)i_this)->create_heap();
 }
 
 /* 0000009C-000001F8       .text create_heap__Q29daObjAuzu5Act_cFv */
-void daObjAuzu::Act_c::create_heap() {
-    /* Nonmatching */
+bool daObjAuzu::Act_c::create_heap() {
+    bool create_result = false;
+    J3DModelData* mdl_data = static_cast<J3DModelData *>(dComIfG_getObjectRes(M_arcname, 0x4));
+    JUT_ASSERT(0xE2, mdl_data != NULL);
+    field_0x298 = mDoExt_J3DModel__create(mdl_data, 0x80000, 0x11000222);
+
+    J3DAnmTextureSRTKey* btk_data = static_cast<J3DAnmTextureSRTKey *>(dComIfG_getObjectRes(M_arcname, 0x7));
+    JUT_ASSERT(0xEC, btk_data != NULL);
+
+    int init_result = field_0x29C.init(
+        mdl_data, btk_data, TRUE, 
+        J3DFrameCtrl::EMode_LOOP, attr().m0C, 
+        0, -1, false, FALSE
+    );
+
+    if (field_0x298 && init_result != 0) {
+        create_result = true;
+    }
+
+    return create_result;
 }
 
 /* 000001F8-000003B8       .text _create__Q29daObjAuzu5Act_cFv */
 cPhs_State daObjAuzu::Act_c::_create() {
-    /* Nonmatching */
+    cPhs_State state = cPhs_ERROR_e;
+    field_0x2B0 = prm_get_type();
+    fopAcM_SetupActor(this, daObjAuzu::Act_c);
+
+    field_0x2B9 = is_exist();
+    if (field_0x2B9 != 0) {
+        state = dComIfG_resLoad(&field_0x290, M_arcname);
+        if (state == cPhs_COMPLEATE_e) {
+            if (fopAcM_entrySolidHeap(this, solidHeapCB, 3456)) {
+                fopAcM_SetMtx(this, field_0x298->getBaseTRMtx());
+                init_mtx();
+                f32 cull_xz = attr().m00 * 3000.0f;
+                fopAcM_setCullSizeBox(this, 
+                    -cull_xz, -30.0f, -cull_xz, 
+                    cull_xz, 30.0f, cull_xz
+                );
+                field_0x2B8 = 0;
+
+                    // TODO: Implement this inline
+                if (daObj::PrmAbstract(this, 1, 16) == 1) {
+                    field_0x2B4 = 0.0f;
+                } else {
+                    if (fopAcM_isSwitch(this, prm_get_swSave())) {
+                        field_0x2B4 = 0.0f;
+                    } else {
+                        field_0x2B4 = 1.0f;
+                    }
+                }
+
+                field_0x2BA = 0;
+                field_0x2BC = -1;
+                set_state_map();
+            } else {
+                state = cPhs_ERROR_e;
+            }
+        }
+    }
+
+    return state;
 }
 
 /* 0000045C-00000498       .text _delete__Q29daObjAuzu5Act_cFv */
 bool daObjAuzu::Act_c::_delete() {
-    /* Nonmatching */
+    if (field_0x2B9 != 0) {
+        dComIfG_resDelete(& field_0x290, M_arcname);
+    }
+    return true;
 }
 
 /* 00000498-000004E8       .text is_exist__Q29daObjAuzu5Act_cCFv */
-void daObjAuzu::Act_c::is_exist() const {
-    /* Nonmatching */
+bool daObjAuzu::Act_c::is_exist() const {
+    if (field_0x2B0 == 0) {
+        return dComIfGs_isEventBit(dSv_event_flag_c::ENDLESS_NIGHT);
+    }
+        
+    return true;
 }
 
 /* 000004E8-00000554       .text set_mtx__Q29daObjAuzu5Act_cFv */
 void daObjAuzu::Act_c::set_mtx() {
-    /* Nonmatching */
+    mDoMtx_stack_c::transS(current.pos);
+    mDoMtx_stack_c::ZXYrotM(shape_angle);
+    field_0x298->setBaseTRMtx(mDoMtx_stack_c::get());
 }
 
 /* 00000554-000005B8       .text init_mtx__Q29daObjAuzu5Act_cFv */
 void daObjAuzu::Act_c::init_mtx() {
-    /* Nonmatching */
+    field_0x298->setBaseScale(
+        cXyz(
+            scale.x * attr().m00,
+            scale.y,
+            scale.z * attr().m00
+        )
+    );
+    set_mtx();
 }
 
 /* 000005B8-00000624       .text set_state_map__Q29daObjAuzu5Act_cFv */
 void daObjAuzu::Act_c::set_state_map() {
     /* Nonmatching */
+    if (daObj::PrmAbstract(this, 1, 16) != 1) {
+        if (is_appear()) {
+            fopAcM_OnStatus(this, fopAcStts_SHOWMAP_e);
+        } else {
+            fopAcM_OffStatus(this, fopAcStts_SHOWMAP_e);
+        }
+    }
 }
 
 /* 00000624-00000788       .text ship_whirl__Q29daObjAuzu5Act_cFv */
@@ -55,7 +146,10 @@ void daObjAuzu::Act_c::ship_whirl() {
 
 /* 00000788-000007DC       .text bgm_start__Q29daObjAuzu5Act_cFv */
 void daObjAuzu::Act_c::bgm_start() {
-    /* Nonmatching */
+    if (field_0x2B0 == 0 && !field_0x2BA) {
+        field_0x2BA = 1;
+        mDoAud_subBgmStart(0x8000002B);
+    }
 }
 
 /* 000007DC-00000AF8       .text _execute__Q29daObjAuzu5Act_cFv */
@@ -70,7 +164,14 @@ void daObjAuzu::Act_c::set_material(J3DMaterial*, unsigned char) {
 
 /* 00000B88-00000C38       .text _draw__Q29daObjAuzu5Act_cFv */
 bool daObjAuzu::Act_c::_draw() {
-    /* Nonmatching */
+    g_env_light.settingTevStruct(TEV_TYPE_BG1, &current.pos, &tevStr);
+    g_env_light.setLightTevColorType(field_0x298, &tevStr);
+    field_0x29C.entry(field_0x298->getModelData());
+                                                                                    // seems kinda superfluous but ok mwcc
+    set_material(field_0x298->getModelData()->getJointNodePointer(0)->getMesh(), u8(field_0x2B4 * 255.5f) & 0xFF);
+    mDoExt_modelUpdateDL(field_0x298);
+
+    return true;
 }
 
 namespace daObjAuzu {
