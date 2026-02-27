034b90bfdc9a71c1a1ae6f46ba5a3fc46eccf340
Roland <roland@wildpaws.xyz>
2026-02-27 13:04:36 -0600

    d_a_daiocta and d_a_daiocta_eye matching (#934)
    * Initial work; 25% matching in .text
    
    * 27% matching in .text
    
    * Match daDaiocta_c::modeProc
    
    * 56% matching in .text
    
    * 61% matching in .text
    
    * 69% matching in .text
    
    * 74% matching in .text
    
    * 84% matching in .text
    
    * daDaiocta_Eye_c 31%
    
    * Match daDaiocta_Eye_c node callbacks
    
    * daDaiocta_Eye_c 69%
    
    * daDaiocta_Eye_c 85%
    
    * daDaiocta_Eye_c 100% for retail and demo
    
    * daDaiocta_c 96%
    
    * daDaiocta_c 100% for retail and demo
    
    * Field name cleanup
    
    * More field naming
    
    * Implement {is_, to_, to_dis}appear daObjAuzu inlines
    
    * Misc code formatting changes
    
    * Use JntHitType enum (d_a_daiocta)
    
    * Apply suggestions from code review
    
    Co-authored-by: LagoLunatic <LagoLunatic@users.noreply.github.com>
    
    * Use fopAcM_SetMtx inline
    
    ---------
    
    Co-authored-by: LagoLunatic <LagoLunatic@users.noreply.github.com>

--- include/d/actor/d_a_obj_auzu.h
+++ include/d/actor/d_a_obj_auzu.h
@@ -9,14 +9,14 @@ namespace daObjAuzu {
     class Act_c : public fopAc_ac_c {
     public:
         void attr() const {}
-        void is_appear() const {}
+        bool is_appear() const { return field_0x2B4 > 0.999f; }
         void prm_get_appear() const {}
         void prm_get_linkID() const {}
         void prm_get_swSave() const {}
         void prm_get_type() const {}
         void prm_make_squid() {}
-        void to_appear() {}
-        void to_disappear() {}
+        void to_appear() { field_0x2B8 = true; }
+        void to_disappear() { field_0x2B8 = false; }
     
         void solidHeapCB(fopAc_ac_c*);
         void create_heap();
@@ -33,8 +33,11 @@ namespace daObjAuzu {
         bool _draw();
     
     public:
-        /* Place member variables here */
-    };
+        /* 0x290 */ u8 field_0x290[0x2B4 - 0x290];
+        /* 0x2B4 */ f32 field_0x2B4;
+        /* 0x2B8 */ bool field_0x2B8;
+        /* 0x2B9 */ u8 field_0x2B9[0x2C0 - 0x2B9];
+    };  // Size: 0x2C0
 };
 
 #endif /* D_A_OBJ_AUZU_H */
