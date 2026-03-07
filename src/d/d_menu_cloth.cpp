2f276439a001b85b93e84bac4ef541cc6f00a4a5
Briggs Baltzell <briggsbaltzellb3@gmail.com>
2026-03-06 19:56:29 -0600

    Add demo version-specific mipmap handling logic in `d_menu_cloth` initialization.

--- src/d/d_menu_cloth.cpp
+++ src/d/d_menu_cloth.cpp
@@ -232,7 +232,11 @@ void dMCloth_c::init() {
     cloth_init();
 
     ResTIMG* image = (ResTIMG*)JKRArchive::getGlbResource('TIMG', "cloth_piece01.bti", mpArc);
-    JUT_ASSERT(0x212, image != NULL);
+    JUT_ASSERT(VERSION_SELECT(528, 530, 530, 530), image != NULL);
+
+#if VERSION == VERSION_DEMO
+    BOOL mipmap = image->mipmapCount > 1;
+#endif
 
     GXInitTexObj(
         &mTexObj,
@@ -242,7 +246,11 @@ void dMCloth_c::init() {
         GXTexFmt(image->format),
         GXTexWrapMode(image->wrapS),
         GXTexWrapMode(image->wrapT),
+#if VERSION == VERSION_DEMO
+        mipmap
+#else
         image->mipmapCount > 1
+#endif
     );
 
     GXInitTexObjLOD(
