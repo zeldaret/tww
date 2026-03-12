3f72d678742466878661e324d37c02e66f720605
Briggs Baltzell <briggsbaltzellb3@gmail.com>
2026-03-12 16:21:13 -0500

    Update vertex attribute formats in `d_a_hookshot.cpp` for improved correctness.

--- src/d/actor/d_a_hookshot.cpp
+++ src/d/actor/d_a_hookshot.cpp
@@ -68,8 +68,8 @@ void daHookshot_shape::draw() {
     GXClearVtxDesc();
     GXSetVtxDesc(GX_VA_POS, GX_INDEX8);
     GXSetVtxDesc(GX_VA_TEX0, GX_INDEX8);
-    GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_CLR_RGBA, GX_F32, 0);
-    GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_CLR_RGBA, GX_F32, 0);
+    GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
+    GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
     GXSetArray(GX_VA_POS, &l_pos, sizeof(l_pos[0]));
     GXSetArray(GX_VA_TEX0, &l_texCoord, sizeof(l_texCoord[0]));
     dKy_GxFog_set();
