1e31ee5848ae47142f9e2ecafa8e591bc5cce9ab
Briggs Baltzell <briggsbaltzellb3@gmail.com>
2026-03-13 21:11:03 -0500

    Update `d_menu_window` structure: replace `field_0x108` with `mMsgID` and modify `field_0x1B0` type to `u16`

--- include/d/d_menu_window.h
+++ include/d/d_menu_window.h
@@ -10,14 +10,14 @@ public:
     /* 0x00FC */ JKRHeap* parentHeap_0xfc;
     /* 0x0100 */ JKRHeap* childHeap;
     /* 0x0104 */ JKRArchive* arc;
-    /* 0x0108 */ s32 field_0x108;
+    /* 0x0108 */ fpc_ProcID mMsgID;
     /* 0x010C */ char* name[2];
     /* 0x0114 */ char* note[2];
     /* 0x011C */ char* dummy[2];
     /* 0x0124 */ u8 field_0x124[0x128 - 0x124];
     /* 0x0128 */ void* buffer_p[33];
     /* 0x01AC */ u8 field_0x1AC[0x1B0 - 0x1AC];
-    /* 0x01B0 */ s16 field_0x1B0;
+    /* 0x01B0 */ u16 field_0x1B0;
     /* 0x01B2 */ u8 field_0x1B2;
     /* 0x01B3 */ u8 field_0x1B3[0x1B6 - 0x1B3];
     /* 0x01B6 */ u8 mButtonsPressed;
