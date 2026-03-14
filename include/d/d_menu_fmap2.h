328f5050ea974de5768b793e4320939621230c5c
Briggs Baltzell <briggsbaltzellb3@gmail.com>
2026-03-13 20:07:49 -0500

    Add `dDlst_FMAP2_c` and `dDlst_FMAP2GS_c` instances to `dMenu_Fmap2_c` and adjust structure padding

--- include/d/d_menu_fmap2.h
+++ include/d/d_menu_fmap2.h
@@ -9,6 +9,18 @@ struct ResTIMG;
 class aramCmapDat_c;
 class dMenu_FmapSv_c;
 
+class dDlst_FMAP2GS_c : public dDlst_base_c {
+public:
+    virtual ~dDlst_FMAP2GS_c() {}
+    virtual void draw();
+};
+
+class dDlst_FMAP2_c : public dDlst_base_c {
+public:
+    virtual ~dDlst_FMAP2_c() {}
+    virtual void draw();
+};
+
 class dMenu_Fmap2_c {
 public:
     virtual ~dMenu_Fmap2_c() {}
@@ -142,20 +154,15 @@ public:
     void isCompleteCollectMap(signed char);
 
 public:
-    /* 0x0004 */ u8 padding_0x4[0x2850 - 0x4];
+    /* 0x0004 */ u8 padding_0x4[0x1C - 0x4];
+    /* 0x001C */ void* mpFmapDatPnt;
+    /* 0x0020 */ dDlst_FMAP2_c mDlst;
+    /* 0x0024 */ u8 padding_0x24[0x28 - 0x24];
+    /* 0x0028 */ dDlst_FMAP2GS_c mDlstGs;
+    /* 0x002C */ u8 padding_0x2C[0x2850 - 0x2C];
 };
 
 STATIC_ASSERT(sizeof(dMenu_Fmap2_c) == 0x2850);
-
-class dDlst_FMAP2_c {
-public:
-    void draw();
-};
-
-class dDlst_FMAP2GS_c {
-public:
-    void draw();
-};
 
 class dMf2_HIO_c {
 public:
