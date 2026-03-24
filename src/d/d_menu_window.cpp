fdf0c028b07d39e6824af1c86a37f4345fd9ca63
Briggs Baltzell <briggsbaltzellb3@gmail.com>
2026-03-23 22:40:00 -0500

    Handle `MW_STATUS_UNK_15` logic by adding condition checks, updating menu state, initializing heap, and triggering audio cues in `d_menu_window`.

--- src/d/d_menu_window.cpp
+++ src/d/d_menu_window.cpp
@@ -939,7 +939,18 @@ static BOOL dMs_Execute(sub_ms_screen_class* i_Ms) {
 
     case MW_STATUS_UNK_15: {
         cloth_c->cloth_move();
-        // TODO
+        if (dMc_c->mCollectMode != 5) {
+            cloth_c->alpha_out();
+            mDoExt_setCurrentHeap(i_Ms->childHeap);
+            i_Ms->mMenuProc = MW_STATUS_UNK_20;
+            dMc_c->m27EC = 0;
+            dMc_c->m27E2 = 10;
+            dMenu_setPushMenuButton(0);
+            mDoAud_seStart(JA_SE_ITM_MENU_OUT);
+        } else {
+            mDoExt_setCurrentHeap(i_Ms->childHeap);
+            dMc_c->_move3();
+        }
     } break;
 
     case MW_STATUS_UNK_5: {
