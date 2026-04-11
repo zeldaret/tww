24102963a0d82681f3fd331f792b0f8f8111640a
meat <noskap@outlook.com>
2026-04-11 13:40:37 +0930

    Remove misleading mWaveTimer name, fix regression in d_magma

--- include/d/d_magma.h
+++ include/d/d_magma.h
@@ -25,7 +25,7 @@ public:
     /* 0x1A */ u8 mTimer;
     /* 0x1C */ Mtx mPosMtx;
     /* 0x4C */ Mtx mTexProjMtx;
-    /* 0x7C */ s16 mWaveTimer;
+    /* 0x7C */ s16 field_0x7C;
 };
 
 class dMagma_ballPath_c : public dMagma_ball_c {
@@ -36,6 +36,8 @@ public:
     virtual void update();
     virtual void setup(f32, u8, int);
 
+public:
+    /* 0x7C */ u8 mWaveTimer;
 };
 
 class dMagma_ballBoss_c : public dMagma_ball_c {
