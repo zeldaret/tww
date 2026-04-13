217c89b76bfe79925865d934942160c335321ad9
Teotia444 <65604571+Teotia444@users.noreply.github.com>
2026-04-13 22:41:59 +0200

    d_a_obj_gtaki (#950)
    * d_a_obj_gtaki - 99% matching
    
    * d_a_obj_gtaki - 99% matching
    
    * d_a_obj_gtaki - small mistakes

--- include/d/actor/d_a_obj_gtaki.h
+++ include/d/actor/d_a_obj_gtaki.h
@@ -1,7 +1,10 @@
 #ifndef D_A_OBJ_GTAKI_H
 #define D_A_OBJ_GTAKI_H
 
+#include "d/d_bg_w.h"
+#include "d/d_cc_d.h"
 #include "f_op/f_op_actor.h"
+#include "m_Do/m_Do_ext.h"
 
 class daObjGtaki_c : public fopAc_ac_c {
 public:
@@ -12,12 +15,19 @@ public:
     void set_effect() {}
 
     void setDummyTexture();
-    void CreateHeap();
-    void CreateInit();
+    BOOL CreateHeap();
+    bool CreateInit();
     void set_mtx();
 
 public:
-    /* Place member variables here */
-};
+    /* 0x290 */ request_of_phase_process_class mPhase;
+    /* 0x298 */ J3DModel* mpModel;
+    /* 0x29C */ mDoExt_btkAnm mBtkAnm;
+    /* 0x2B0 */ dCcD_Stts mStts;
+    /* 0x2EC */ dCcD_Cyl mCyl;
+    /* 0x41C */ dBgW* mpBgW;
+    /* 0x420 */ Mtx mMtx;
+    /* 0x450 */ dKy_tevstr_c mTevStr;
+};  // Size: 0x500
 
 #endif /* D_A_OBJ_GTAKI_H */
