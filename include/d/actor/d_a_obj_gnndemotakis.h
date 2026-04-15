87afcee02b20b6bab132b530607dd3018a0cf199
jaysc <9077125+jaysc@users.noreply.github.com>
2026-04-16 00:03:45 +0900

    init

--- include/d/actor/d_a_obj_gnndemotakis.h
+++ include/d/actor/d_a_obj_gnndemotakis.h
@@ -2,19 +2,25 @@
 #define D_A_OBJ_GNNDEMOTAKIS_H
 
 #include "f_op/f_op_actor.h"
+#include "m_Do/m_Do_ext.h"
 
 class daObjGnntakis_c : public fopAc_ac_c {
 public:
-    void solidHeapCB(fopAc_ac_c*);
-    void create_heap();
+    static BOOL solidHeapCB(fopAc_ac_c*);
+    BOOL create_heap();
     cPhs_State _create();
     bool _delete();
     void init_mtx();
     bool _execute();
     bool _draw();
 
+    static const char m_arcname[];
+
 public:
-    /* Place member variables here */
-};
+    /* 0x290 */ J3DModel* mpModel;
+    /* 0x294 */ request_of_phase_process_class mPhs;
+    /* 0x29C */ mDoExt_btkAnm mpBtkAnm;
+    /* 0x2B0 */ BOOL m2B0;
+};  // Size: 0x2B4
 
 #endif /* D_A_OBJ_GNNDEMOTAKIS_H */
