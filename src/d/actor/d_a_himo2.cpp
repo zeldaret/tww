fc1c01ee39f06dd4514aca5937a4afebedff5510
banteg <4562643+banteg@users.noreply.github.com>
2026-05-12 02:45:43 +0400

    fix: align himo2 control pointer lifetime

--- src/d/actor/d_a_himo2.cpp
+++ src/d/actor/d_a_himo2.cpp
@@ -232,17 +232,19 @@ void himo2_control2(himo2_class* i_this, himo2_s* param_2) {
 
     local_a8.x = 0.0f;
     local_a8.y = 0.0f;
+    himo2_s* puVar6 = param_2;
+    puVar6 += 98;
     if (i_this->m02DC == 0) {
         local_a8.z = i_this->m2188 * 15.625f * (REG8_F(18) + 0.002f);
     } else {
         local_a8.z = i_this->m2188 * 15.625f * (REG0_F(1) + 0.0007f);
     }
     f32 dVar11;
-    f32 dVar10 = 0.0f;
-    f32 dVar9 = 0.0f;
+    f32 dVar10;
+    f32 dVar9;
+    dVar10 = dVar9 = 0.0f;
     f32 dVar8 = (REG8_F(17) + 10.0f);
     int iVar4 = i_this->m02CC + 1;
-    himo2_s* puVar6 = param_2 + 98;
     for (; iVar4 < 100; iVar4++, puVar6--) {
         dVar11 = (puVar6->m10.y - puVar6[1].m10.y);
         int iVar3 = i_this->m02DC;
