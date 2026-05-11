1789146d2286162087781126859faa5a6f661e57
banteg <4562643+banteg@users.noreply.github.com>
2026-05-11 23:40:39 +0400

    fix: align himo2 endpoint loop counters

--- src/d/actor/d_a_himo2.cpp
+++ src/d/actor/d_a_himo2.cpp
@@ -486,8 +486,8 @@ static BOOL daHimo2_Draw(himo2_class* i_this) {
             local_a4c.x = local_a4c.x * 0.06666667f;
             local_a4c.y = local_a4c.y * 0.06666667f;
             local_a4c.z = local_a4c.z * 0.06666667f;
-            int uVar6 = 0;
             int uVar4 = 0;
+            int uVar6 = 0;
             for (int i = 0; i < 16; i++) {
                 f32 fVar1 = cM_ssin(uVar4);
                 pcVar7->x = i_this->m02EC[1].x + local_a4c.x * uVar6 + local_a28.x * fVar1;
