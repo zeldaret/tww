1c55582ff743f75388fb315f7c1d7ab84c5742d3
banteg <4562643+banteg@users.noreply.github.com>
2026-05-13 01:23:37 +0400

    decomp: refine daPz eye control

--- src/d/actor/d_a_pz.cpp
+++ src/d/actor/d_a_pz.cpp
@@ -1061,10 +1061,10 @@ void daPz_c::ctrlEye() {
     s16 eye_x = target_y - (s16)(shape_angle.y + m_jnt.getHead_y() + m_jnt.getBackbone_y());
     s16 eye_y = target_x - (s16)(m_jnt.getHead_x() + m_jnt.getBackbone_x());
 
-    f32 eye_y_rate = (s16)eye_y / 8192.0f;
     f32 eye_x_rate = (s16)eye_x / 8192.0f;
-    f32 y_offset = 0.1f * eye_y_rate;
-    f32 x_offset = 0.1f * eye_x_rate;
+    f32 eye_y_rate = (s16)eye_y / 8192.0f;
+    f32 x_offset = eye_x_rate * 0.1f;
+    f32 y_offset = eye_y_rate * 0.1f;
 
     m06DC = (int)cLib_checkMinMaxLimit(y_offset, -0.1f, 0.1f);
     m06DC = (int)cLib_checkMinMaxLimit(x_offset, -0.1f, 0.1f);
