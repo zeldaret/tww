55f7c0c3ff8a5cda293f83dee93dd53a2a756599
banteg <4562643+banteg@users.noreply.github.com>
2026-05-13 10:07:46 +0400

    decomp: improve daPz eye control match

--- src/d/actor/d_a_pz.cpp
+++ src/d/actor/d_a_pz.cpp
@@ -1060,12 +1060,13 @@ void daPz_c::ctrlEye() {
     s16 target_x = cLib_targetAngleX(&mHeadFrontPos, &mEyeTarget);
     s16 target_y = cLib_targetAngleY(&mHeadFrontPos, &mEyeTarget);
 
-    csXyz eye_angle;
-    eye_angle.x = target_y - (s16)(shape_angle.y + m_jnt.getHead_y() + m_jnt.getBackbone_y());
-    eye_angle.y = target_x - (s16)(m_jnt.getHead_x() + m_jnt.getBackbone_x());
+    s16 eye_yaw = target_y;
+    eye_yaw -= (s16)(shape_angle.y + m_jnt.getHead_y() + m_jnt.getBackbone_y());
+    s16 eye_pitch = target_x;
+    eye_pitch -= (s16)(m_jnt.getHead_x() + m_jnt.getBackbone_x());
 
-    f32 eye_x_rate = (s16)eye_angle.x / 8192.0f;
-    f32 eye_y_rate = (s16)eye_angle.y / 8192.0f;
+    f32 eye_y_rate = (s16)eye_pitch / 8192.0f;
+    f32 eye_x_rate = (s16)eye_yaw / 8192.0f;
     f32 x_offset = eye_x_rate;
     f32 y_offset = eye_y_rate;
     y_offset *= 0.1f;
