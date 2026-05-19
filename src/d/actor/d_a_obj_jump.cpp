bcec9cf3cd4a92fa48f38a4a82ae953557f973ef
marian <marian.muller@serli.com>
2026-05-19 23:10:24 +0200

    clean unnecessary return and this->

--- src/d/actor/d_a_obj_jump.cpp
+++ src/d/actor/d_a_obj_jump.cpp
@@ -95,10 +95,10 @@ BOOL daObjJump::Act_c::CreateHeap() {
 /* 00000184-0000033C       .text Create__Q29daObjJump5Act_cFv */
 BOOL daObjJump::Act_c::Create() {
     unsigned int uVar2;
-    this->field_0x338 = 1.0f;
-    this->field_0x33C = 1.0f;
-    this->cullMtx = this->mModel->getBaseTRMtx();
-    this->init_mtx();
+    field_0x338 = 1.0f;
+    field_0x33C = 1.0f;
+    cullMtx = mModel->getBaseTRMtx();
+    init_mtx();
     fopAcM_setCullSizeBox(
         this,
         attr().cullSizeBoxMin.x,
@@ -110,26 +110,26 @@ BOOL daObjJump::Act_c::Create() {
     );
     if (attr().field_0x014 != 0) {
         cXyz temp(current.pos.x, current.pos.y + 50.0f, current.pos.z);
-        this->field_0x2D8.SetPos(&temp);
+        field_0x2D8.SetPos(&temp);
         uVar2 = fopAcM_GetID(this);
-        this->field_0x2D8.SetActorPid(uVar2);
-        this->field_0x32C = dComIfG_Bgsp()->GroundCross(&this->field_0x2D8);
+        field_0x2D8.SetActorPid(uVar2);
+        field_0x32C = dComIfG_Bgsp()->GroundCross(&field_0x2D8);
     }
-    if (this->field_0x2D4 == 1) {
-        this->actor_status &= 0xffffffc0;
-        this->gbaName = 0;
+    if (field_0x2D4 == 1) {
+        actor_status &= 0xffffffc0;
+        gbaName = 0;
     }
-    this->mpBgW->SetRideCallback(rideCB);
-    this->mode_wait_init();
+    mpBgW->SetRideCallback(rideCB);
+    mode_wait_init();
     return TRUE;
 }
 
 /* 0000033C-00000544       .text Mthd_Create__Q29daObjJump5Act_cFv */
 cPhs_State daObjJump::Act_c::Mthd_Create() {
     fopAcM_ct(this, daObjJump::Act_c);
-    cPhs_State phase_state = dComIfG_resLoad(&this->mPhase, M_arcname);
+    cPhs_State phase_state = dComIfG_resLoad(&mPhase, M_arcname);
     if (phase_state == cPhs_COMPLEATE_e) {
-        this->field_0x2D4 = prm_get_type();
+        field_0x2D4 = prm_get_type();
         phase_state = MoveBGCreate(
             M_arcname,
             attr().resIndex,
@@ -160,7 +160,7 @@ void daObjJump::Act_c::set_mtx() {
     mModel->setBaseTRMtx(mDoMtx_stack_c::get());
     float fVar1 = attr().field_0x020 - attr().field_0x024;
     float fVar2 = attr().field_0x01C - attr().field_0x018;
-    float fVar3 = (this->field_0x33C - 1.0f);
+    float fVar3 = (field_0x33C - 1.0f);
     float fVar4 = (fVar1 + fVar2 * fVar3) / fVar1;
     mDoMtx_stack_c::transM(0.0f, attr().field_0x024, 0.0f);
     mDoMtx_stack_c::scaleM(1.0f, fVar4,1.0f);
@@ -170,87 +170,83 @@ void daObjJump::Act_c::set_mtx() {
 
 /* 00000B78-00000BB4       .text init_mtx__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::init_mtx() {
-    this->mModel->setBaseScale(this->scale);
-    this->set_mtx();
-    return;
+    mModel->setBaseScale(scale);
+    set_mtx();
 }
 
 /* 00000BB4-00000D9C       .text set_push_flag__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::set_push_flag() {
-    this->field_0x345 = 0;
-    if (this->field_0x344 != 0) {
-        if (this->field_0x347 < 0xff) {
-            this->field_0x347 += 1;
+    field_0x345 = 0;
+    if (field_0x344 != 0) {
+        if (field_0x347 < 0xff) {
+            field_0x347 += 1;
         }
-        if (this->field_0x347 == attr().field_0x043) {
-            this->field_0x345 = 1;
-        }
-    } else {
-        this->field_0x347 = 0;
-    }
-    this->field_0x346 = 0;
-    if (this->field_0x344 != 0) {
-        this->field_0x348 = 0;
-    } else {
-        if (this->field_0x348 < 0xff) {
-            this->field_0x348 += 1;
-        }
-        if (this->field_0x348 == attr().field_0x044) {
-            this->field_0x346 = 1;
-        }
-    }
-    this->field_0x34a = 0;
-    if (this->field_0x349 != 0 && dComIfGp_checkPlayerStatus0(0, daPyStts0_UNK100_e)) {
-        if (this->field_0x34b < 0xff) {
-            this->field_0x34b += 1;
-        }
-        if (this->field_0x34b == attr().field_0x045) {
-            this->field_0x34a = 1;
+        if (field_0x347 == attr().field_0x043) {
+            field_0x345 = 1;
         }
     } else {
-        this->field_0x34b = 0;
+        field_0x347 = 0;
     }
-    this->field_0x34d = 0;
-    if (this->field_0x34c != 0) {
-        if (this->field_0x34f < 0xff) {
-            this->field_0x34f += 1;
+    field_0x346 = 0;
+    if (field_0x344 != 0) {
+        field_0x348 = 0;
+    } else {
+        if (field_0x348 < 0xff) {
+            field_0x348 += 1;
         }
-        if (this->field_0x34f == attr().field_0x046) {
-            this->field_0x34d = 1;
+        if (field_0x348 == attr().field_0x044) {
+            field_0x346 = 1;
+        }
+    }
+    field_0x34a = 0;
+    if (field_0x349 != 0 && dComIfGp_checkPlayerStatus0(0, daPyStts0_UNK100_e)) {
+        if (field_0x34b < 0xff) {
+            field_0x34b += 1;
+        }
+        if (field_0x34b == attr().field_0x045) {
+            field_0x34a = 1;
         }
     } else {
-        this->field_0x34f = 0;
+        field_0x34b = 0;
     }
-    this->field_0x34e = 0;
-    if (this->field_0x34c != 0) {
-        this->field_0x350 = 0;
+    field_0x34d = 0;
+    if (field_0x34c != 0) {
+        if (field_0x34f < 0xff) {
+            field_0x34f += 1;
+        }
+        if (field_0x34f == attr().field_0x046) {
+            field_0x34d = 1;
+        }
+    } else {
+        field_0x34f = 0;
+    }
+    field_0x34e = 0;
+    if (field_0x34c != 0) {
+        field_0x350 = 0;
         return;
     }
-    if (this->field_0x350 < 0xff) {
-        this->field_0x350 += 1;
+    if (field_0x350 < 0xff) {
+        field_0x350 += 1;
     }
-    if (this->field_0x350 != attr().field_0x047) {
+    if (field_0x350 != attr().field_0x047) {
         return;
     }
-    this->field_0x34e = 1;
-    return;
+    field_0x34e = 1;
 }
 
 /* 00000D9C-00000DB4       .text clear_push_flag__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::clear_push_flag() {
-    this->field_0x344 = 0;
-    this->field_0x34c = 0;
-    this->field_0x349 = 0;
-    this->field_0x354 = 0;
-    return;
+    field_0x344 = 0;
+    field_0x34c = 0;
+    field_0x349 = 0;
+    field_0x354 = 0;
 }
 
 /* 00000DB4-00000E1C       .text calc_vib_pos__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::calc_vib_pos() {
-    this->field_0x340 = this->field_0x340 - (this->field_0x33C - this->field_0x338) * attr().field_0x02C;
-    this->field_0x340 = this->field_0x340 - this->field_0x340 * attr().field_0x028;
-    this->field_0x33C = this->field_0x33C + this->field_0x340;
-    return;
+    field_0x340 = field_0x340 - (field_0x33C - field_0x338) * attr().field_0x02C;
+    field_0x340 = field_0x340 - field_0x340 * attr().field_0x028;
+    field_0x33C = field_0x33C + field_0x340;
 }
 
 /* 00000E1C-00000E74       .text rideCB__Q29daObjJump5Act_cFP4dBgWP10fopAc_ac_cP10fopAc_ac_c */
@@ -269,7 +265,6 @@ void daObjJump::Act_c::rideCB(dBgW*, fopAc_ac_c* param_2, fopAc_ac_c* param_3) {
     if (fopAcM_GetSpeedF(player) > a->field_0x04C) {
         pvVar4->field_0x354 = 1;
     }
-    return;
 }
 
 /* 00000E74-00000F48       .text jnodeCB_lower__Q29daObjJump5Act_cFP7J3DNodei */
@@ -291,124 +286,112 @@ BOOL daObjJump::Act_c::jnodeCB_lower(J3DNode* param_1, int param_2) {
 
 /* 00000F48-00000F64       .text mode_wait_init__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_wait_init() {
-    this->field_0x330 = 0;
-    this->field_0x338 = 1.0f;
-    this->field_0x352 = 0;
-    return;
+    field_0x330 = 0;
+    field_0x338 = 1.0f;
+    field_0x352 = 0;
 }
 
 /* 00000F64-0000108C       .text mode_wait__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_wait() {
-    if (this->field_0x352 <= 0 && (this->field_0x345 != 0 || this->field_0x346 != 0 || this->field_0x34a != 0 || this->field_0x34d != 0)) {
-        this->field_0x340 += attr().field_0x030;
-        this->field_0x352 = attr().field_0x048;
+    if (field_0x352 <= 0 && (field_0x345 != 0 || field_0x346 != 0 || field_0x34a != 0 || field_0x34d != 0)) {
+        field_0x340 += attr().field_0x030;
+        field_0x352 = attr().field_0x048;
     }
-    else if (this->field_0x352 > 0) {
-        this->field_0x352 -= 1;
+    else if (field_0x352 > 0) {
+        field_0x352 -= 1;
     }
-    if (this->field_0x34f > attr().field_0x040) {
+    if (field_0x34f > attr().field_0x040) {
         fopAcM_seStart(this, JA_SE_OBJ_JUMP_SPR_CLOSE, 0);
-        this->mode_w_l_init();
+        mode_w_l_init();
     }
-    return;
 }
 
 /* 0000108C-000010B4       .text mode_w_l_init__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_w_l_init() {
-    this->field_0x330 = 1;
-    this->field_0x338 = attr().field_0x038;
-    return;
+    field_0x330 = 1;
+    field_0x338 = attr().field_0x038;
 }
 
 /* 000010B4-000010E8       .text mode_w_l__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_w_l() {
-    if (this->field_0x33C <= this->field_0x338) {
-        this->mode_lower_init();
+    if (field_0x33C <= field_0x338) {
+        mode_lower_init();
     }
-    return;
 }
 
 /* 000010E8-000010FC       .text mode_lower_init__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_lower_init() {
-    this->field_0x330 = 2;
-    this->field_0x352 = 0;
-    return;
+    field_0x330 = 2;
+    field_0x352 = 0;
 }
 
 /* 000010FC-00001200       .text mode_lower__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_lower() {
-    if (this->field_0x352 <= 0 && this->field_0x34e != 0) {
-        this->field_0x340 += attr().field_0x030;
-        this->field_0x352 = attr().field_0x048;
+    if (field_0x352 <= 0 && field_0x34e != 0) {
+        field_0x340 += attr().field_0x030;
+        field_0x352 = attr().field_0x048;
     }
-    else if (this->field_0x352 > 0) {
-        this->field_0x352 -= 1;
+    else if (field_0x352 > 0) {
+        field_0x352 -= 1;
     }
-    if (this->field_0x350 > attr().field_0x041) {
+    if (field_0x350 > attr().field_0x041) {
         fopAcM_seStart(this, JA_SE_OBJ_JUMP_SPR_OPEN, 0);
-        this->mode_l_u_init();
+        mode_l_u_init();
     }
-    return;
 }
 
 /* 00001200-00001228       .text mode_l_u_init__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_l_u_init() {
-    this->field_0x330 = 3;
-    this->field_0x338 = attr().field_0x03C;
-    return;
+    field_0x330 = 3;
+    field_0x338 = attr().field_0x03C;
 }
 
 /* 00001228-00001290       .text mode_l_u__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_l_u() {
-    if (this->field_0x33C >= this->field_0x338) {
-        this->field_0x33C = this->field_0x338;
-        this->field_0x340 = 0.0f;
-        if (*(u8*)(&this->field_0x349) != 0) {
+    if (field_0x33C >= field_0x338) {
+        field_0x33C = field_0x338;
+        field_0x340 = 0.0f;
+        if (*(u8*)(&field_0x349) != 0) {
             daPy_py_c* player = daPy_getPlayerActorClass();
             player->onForceVomitJump();
         }
-        this->mode_upper_init();
+        mode_upper_init();
     }
-    return;
 }
 
 /* 00001290-000012B8       .text mode_upper_init__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_upper_init() {
-    this->field_0x330 = 4;
-    this->field_0x334 = attr().field_0x042;
-    return;
+    field_0x330 = 4;
+    field_0x334 = attr().field_0x042;
 }
 
 /* 000012B8-000012EC       .text mode_upper__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_upper() {
-    s16 var = this->field_0x334;
-    this->field_0x334 = var - 1;
+    s16 var = field_0x334;
+    field_0x334 = var - 1;
     if ((s16)(var - 1) <= 0) {
         mode_u_w_init();
     }
-    return;
 }
 
 /* 000012EC-00001304       .text mode_u_w_init__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_u_w_init() {
-    this->field_0x330 = 5;
-    this->field_0x338 = 1.0f;
-    return;
+    field_0x330 = 5;
+    field_0x338 = 1.0f;
 }
 
 /* 00001304-00001338       .text mode_u_w__Q29daObjJump5Act_cFv */
 void daObjJump::Act_c::mode_u_w() {
-    if (this->field_0x33C <= this->field_0x338) {
-        this->mode_wait_init();
+    if (field_0x33C <= field_0x338) {
+        mode_wait_init();
     }
-    return;
 }
 
 /* 00001338-000014B0       .text Execute__Q29daObjJump5Act_cFPPA3_A4_f */
 BOOL daObjJump::Act_c::Execute(Mtx** param_1) {
     /* Nonmatching */ 
 
-    this->set_push_flag();
+    set_push_flag();
 
     typedef void (daObjJump::Act_c::*procFunc)();
     static procFunc mode_proc[] = {
@@ -420,15 +403,15 @@ BOOL daObjJump::Act_c::Execute(Mtx** param_1) {
         &daObjJump::Act_c::mode_u_w
     };
 
-    (this->*mode_proc[this->field_0x330])();
+    (this->*mode_proc[field_0x330])();
 
-    if ((this->field_0x330 == 0 || this->field_0x330 == 2) && this->field_0x354 != 0) {
-        this->field_0x340 += attr().field_0x034;
+    if ((field_0x330 == 0 || field_0x330 == 2) && field_0x354 != 0) {
+        field_0x340 += attr().field_0x034;
     }
-    this->calc_vib_pos();
-    this->set_mtx();
+    calc_vib_pos();
+    set_mtx();
     *param_1 = &M_tmp_mtx;
-    this->clear_push_flag();
+    clear_push_flag();
     return TRUE;
 }
 
@@ -436,17 +419,17 @@ BOOL daObjJump::Act_c::Execute(Mtx** param_1) {
 BOOL daObjJump::Act_c::Draw() {
     /* Nonmatching */
     g_env_light.settingTevStruct(TEV_TYPE_ACTOR, &current.pos, &tevStr);
-    g_env_light.setLightTevColorType(this->mModel, &tevStr);
+    g_env_light.setLightTevColorType(mModel, &tevStr);
     dComIfGd_setList();
-    mDoExt_modelUpdateDL(this->mModel);
+    mDoExt_modelUpdateDL(mModel);
     dComIfGd_setListInvisisble();
     if (attr().field_0x014 != 0) {
         dComIfGd_setSimpleShadow2(
             &current.pos,
-            this->field_0x32C,
+            field_0x32C,
             70.0f,
-            this->field_0x2D8,
-            this->shape_angle.y,
+            field_0x2D8,
+            shape_angle.y,
             1.0f,
             NULL
         );
