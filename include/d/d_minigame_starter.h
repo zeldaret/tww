31ae34bf28cfe305cac2e85553ee1060b751e9b8
TakaRikka <takarikka@outlook.com>
2026-06-27 17:37:42 -0700

    d_minigame_starter attempt

--- include/d/d_minigame_starter.h
+++ include/d/d_minigame_starter.h
@@ -1,38 +1,56 @@
 #ifndef D_MINIGAME_STARTER_H
 #define D_MINIGAME_STARTER_H
 
-#include "dolphin/types.h"
-#include "f_op/f_op_msg.h"
+#include "f_op/f_op_msg_mng.h"
+#include "d/d_drawlist.h"
 
 class JKRArchive;
 struct fopMsgM_pane_class;
+class J2DScreen;
 
-class dDlst_StarterScrnDraw_c {
+class dDlst_StarterScrnDraw_c : public dDlst_base_c {
 public:
     void acc(s16, s16, s16) {}
     
     void setScreen(const char*, JKRArchive*);
-    void anime1(int);
-    void anime2();
+    BOOL anime1(int);
+    BOOL anime2();
     void scaleAnime(f32);
     void setRotate(fopMsgM_pane_class*, f32);
-    void draw();
+
+    virtual ~dDlst_StarterScrnDraw_c() {}
+    virtual void draw();
+
+    /* 0x004 */ J2DScreen* mpScrn;
+    #if VERSION > VERSION_JPN
+    /* 0x008 */ fopMsgM_pane_class field_0x008[7];
+    #else
+    /* 0x008 */ fopMsgM_pane_class field_0x008[3];
+    #endif
+    /* 0x190 */ fopMsgM_pane_class field_0x190;
+    /* 0x1C8 */ fopMsgM_pane_class field_0x1c8[3];
+    /* 0x270 */ fopMsgM_pane_class field_0x270[3];
 };
 
 class dMinigame_Starter_c : public msg_class {
 public:
-    void countStart() { if (!field_0x111) field_0x111 = true; }
+    void countStart() { if (!mStatus) mStatus = 1; }
     
-    void _create();
-    void _execute();
-    void _draw();
-    void _delete();
+    int _create();
+    int _execute();
+    int _draw();
+    int _delete();
     BOOL startCheck();
     BOOL deleteCheck();
+
 public:
-    /* 0x0FC */ u8 field_0x0FC[0x111 - 0x0FC];
-    /* 0x111 */ bool field_0x111;
-    /* 0x112 */ u8 field_0x112[0x114 - 0x112];
+    /* 0x0FC */ dDlst_StarterScrnDraw_c* mStarterScrn;
+    /* 0x100 */ request_of_phase_process_class mPhase;
+    /* 0x108 */ JKRSolidHeap* mHeap;
+    /* 0x10C */ s16 mTimer;
+    /* 0x10E */ s16 field_0x10e;
+    /* 0x110 */ u8 field_0x110;
+    /* 0x111 */ u8 mStatus;
 };  // Size: 0x114
 
 #endif /* D_MINIGAME_STARTER_H */
