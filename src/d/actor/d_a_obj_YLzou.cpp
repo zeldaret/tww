a8cd7d98e85d281d9d387d30ac6e1e6e4b4e77cd
Lee, Derek <Dereklee9ab@aol.com>
2026-07-13 01:02:00 -0700

    rename variable to match objdiff

--- src/d/actor/d_a_obj_YLzou.cpp
+++ src/d/actor/d_a_obj_YLzou.cpp
@@ -141,7 +141,7 @@ bool daObjYLzou_c::create_heap() {
 /* 000004F4-00000668       .text eff_set_slip_smoke_pos__12daObjYLzou_cFv */
 void daObjYLzou_c::eff_set_slip_smoke_pos() {
     /* Nonmatching */
-    static cXyz positions[2] = {cXyz(0xC3700000, 0.0f, 0xC3700000), cXyz(0x43700000, 0.0f, 0xC3700000)};
+    static cXyz base_pos[2] = {cXyz(0xC3700000, 0.0f, 0xC3700000), cXyz(0x43700000, 0.0f, 0xC3700000)};
 
     f32 new_y = current.pos.y + -1550;
     f32 new_z = current.pos.z + -1200;
@@ -149,7 +149,7 @@ void daObjYLzou_c::eff_set_slip_smoke_pos() {
     mDoMtx_stack_c::YrotM(current.angle.y);
     for (int i = 0; i < (int)ARRAY_SIZE(mSmokeCbs); ++i) {
         mDoMtx_stack_c::push();
-        mDoMtx_stack_c::transM(positions[i]);
+        mDoMtx_stack_c::transM(base_pos[i]);
         mDoMtx_multVecZero(mDoMtx_stack_c::get(), &mSmokeCbs[i].field_0x20);
         mSmokeCbs[i].field_0x2C.set(0, current.angle.y, 0);
         mDoMtx_stack_c::pop();
