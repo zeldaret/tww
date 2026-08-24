1f41ba855aac9f87e616b0d9ae48442198f17d4a
Hygoto <git@hygoto.de>
2026-08-25 00:06:08 +0200

    implemented short functions and _create

--- include/d/actor/d_a_obj_htetu1.h
+++ include/d/actor/d_a_obj_htetu1.h
@@ -2,27 +2,9 @@
 #define D_A_OBJ_HTETU1_H
 
 #include "f_op/f_op_actor.h"
+#include "d/d_bg_w.h"
+#include "d/d_particle.h"
 
-class daObjHtetu1_c : public fopAc_ac_c {
-public:
-    // TODO: this function is marked as weak in the REL symbol map, but it does not get inlined for some reason?
-    // void check_sw() {}
-    void check_sw();
-
-    void solidHeapCB(fopAc_ac_c*);
-    void create_heap();
-    cPhs_State _create();
-    bool _delete();
-    void init_mtx();
-    void unlock();
-    void get_water_h();
-    void splash_manager();
-    bool _execute();
-    bool _draw();
-
-public:
-    /* Place member variables here */
-};
 
 class daObjHtetu1Splash_c {
 public:
@@ -36,6 +18,54 @@ public:
     void timer_play_particle(short) {}
 
     void create_s(unsigned short, cXyz*, csXyz*, dKy_tevstr_c*);
-};
+
+public:
+    /* 0x00 */ dPa_followEcallBack field_0x00;
+    /* 0x14 */ cXyz mPos;
+    /* 0x20 */ csXyz mRot;
+    /* 0x26 */ u16 field_0x26;
+    /* 0x28 */ u8 field_0x28;
+    /* 0x29 */ u8 field_0x29[0x2C - 0x29];
+};  // Size: 0x2C
+
+
+class daObjHtetu1_c : public fopAc_ac_c {
+public:
+    // TODO: this function is marked as weak in the REL symbol map, but it does not get inlined for some reason?
+    // void check_sw() {}
+    BOOL check_sw();
+
+    static BOOL solidHeapCB(fopAc_ac_c*);
+    BOOL create_heap();
+    cPhs_State _create();
+    bool _delete();
+    void init_mtx();
+    void unlock();
+    void get_water_h();
+    void splash_manager();
+    bool _execute();
+    bool _draw();
+
+public:
+        static const char M_arcname[];
+
+public:
+    /* 0x290 */ J3DModel* mpModel;
+    /* 0x294 */ request_of_phase_process_class mPhase;
+    /* 0x29C */ uint field_0x29C;
+    /* 0x2A0 */ cXyz field_0x2A0;
+    /* 0x2AC */ cXyz field_0x2AC;
+    /* 0x2B8 */ f32 field_0x2B8;
+    /* 0x2BC */ f32 field_0x2BC;
+    /* 0x2C0 */ u16 field_0x2C0;
+    /* 0x2C2 */ u8 field_0x2C2[0x2C4 - 0x2C2];
+    /* 0x2C4 */ s16 field_0x2C4;
+    /* 0x2C6 */ u8 field_0x2C6;
+    /* 0x2C7 */ s8 field_0x2C7;
+    /* 0x2C8 */ s16 field_0x2C8;
+    /* 0x2CA */ u8 field_0x2CA[0x2CC - 0x2CA];
+    /* 0x2CC */ dBgW* field_0x2CC;
+    /* 0x2D0 */ daObjHtetu1Splash_c field_0x2D0[2];
+};  // Size: 0x328
 
 #endif /* D_A_OBJ_HTETU1_H */
--- src/d/actor/d_a_obj_htetu1.cpp
+++ src/d/actor/d_a_obj_htetu1.cpp
@@ -5,6 +5,28 @@
 
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 #include "d/actor/d_a_obj_htetu1.h"
+#include "JSystem/J3DGraphAnimator/J3DModel.h"
+#include "JSystem/J3DGraphAnimator/J3DModelData.h"
+#include "JSystem/JUtility/JUTAssert.h"
+#include "SSystem/SComponent/c_bg_w.h"
+#include "SSystem/SComponent/c_phase.h"
+#include "d/d_bg_w.h"
+#include "d/d_com_inf_game.h"
+#include "d/d_kankyo.h"
+#include "d/d_particle_name.h"
+#include "dolphin/types.h"
+#include "f_op/f_op_actor.h"
+#include "f_op/f_op_actor_mng.h"
+#include "f_pc/f_pc_draw_priority.h"
+#include "m_Do/m_Do_ext.h"
+#include "m_Do/m_Do_mtx.h"
+#include "res/Object/Htetu1.h"
+
+const u16 l_daObjHtetu1_splash_id_table[] = {
+    dPa_name::ID_AK_SN_SIRENWATERGATE00,
+    dPa_name::ID_AK_SN_SIRENWATERGATE01
+};
+const char daObjHtetu1_c::M_arcname[7] = "Htetu1";
 
 /* 00000078-00000178       .text create_s__19daObjHtetu1Splash_cFUsP4cXyzP5csXyzP12dKy_tevstr_c */
 void daObjHtetu1Splash_c::create_s(unsigned short, cXyz*, csXyz*, dKy_tevstr_c*) {
@@ -12,33 +34,81 @@ void daObjHtetu1Splash_c::create_s(unsigned short, cXyz*, csXyz*, dKy_tevstr_c*)
 }
 
 /* 00000178-00000198       .text solidHeapCB__13daObjHtetu1_cFP10fopAc_ac_c */
-void daObjHtetu1_c::solidHeapCB(fopAc_ac_c*) {
+BOOL daObjHtetu1_c::solidHeapCB(fopAc_ac_c *f) {
     /* Nonmatching */
+    return ((daObjHtetu1_c*)f)->create_heap();
 }
 
 /* 00000198-00000298       .text create_heap__13daObjHtetu1_cFv */
-void daObjHtetu1_c::create_heap() {
+BOOL daObjHtetu1_c::create_heap() {
     /* Nonmatching */
+    BOOL ret = TRUE;
+    
+    J3DModelData *mdl_data = static_cast<J3DModelData*>(dComIfG_getObjectRes(M_arcname, dRes_INDEX_HTETU1_BDL_HTETU1_e));
+    JUT_ASSERT(0x119, mdl_data != NULL);
+    if (mdl_data == NULL) ret = FALSE;
+    else {
+        mpModel = mDoExt_J3DModel__create(mdl_data, 0, 0x11020203);
+        Mtx *mdl = &mpModel->getBaseTRMtx();
+        cBgD_t *pcVar3 = (cBgD_t*)dComIfG_getObjectRes(M_arcname, dRes_INDEX_HTETU1_DZB_HTETU1_e);
+        field_0x2CC = dBgW_NewSet(pcVar3, cBgW::MOVE_BG_e, mdl);
+        if (field_0x2CC == NULL) ret = FALSE;
+    }
+    return ret;
 }
 
 /* 00000298-00000460       .text _create__13daObjHtetu1_cFv */
 cPhs_State daObjHtetu1_c::_create() {
     /* Nonmatching */
+    fopAcM_ct(this, daObjHtetu1_c);
+
+    cPhs_State phase_state = dComIfG_resLoad(&mPhase, M_arcname);
+
+    if (phase_state == cPhs_COMPLEATE_e) {
+        phase_state = cPhs_ERROR_e;
+        if (fopAcM_entrySolidHeap(this, solidHeapCB, 0xae0)) {
+            fopAcM_SetMtx(this, mpModel->getBaseTRMtx());
+            field_0x29C = base.base.mParameters & 0xff;
+            field_0x2B8 = current.pos.y - 2300;
+            if (check_sw() != 0) {
+                current.pos.y = field_0x2B8;
+                field_0x2C6 = 2;
+            }
+            field_0x2C8 = -1;
+            init_mtx();
+            g_env_light.settingTevStruct(2, &current.pos, &tevStr);
+
+            for (int i = 0; i < 2; i++) {
+                field_0x2D0[i].create_s(l_daObjHtetu1_splash_id_table[i], &current.pos, &current.angle, &tevStr);
+            }
+            fopAcM_setCullSizeBox(this, -950, -1000, -100, 950, 1300, 100);
+            dComIfG_Bgsp()->Regist(field_0x2CC, this);
+            field_0x2C4 = dComIfGp_evmng_getEventIdx("htetu1_open");
+            phase_state = cPhs_COMPLEATE_e;
+        }
+    }
+    return phase_state;
 }
 
 /* 00000508-00000610       .text _delete__13daObjHtetu1_cFv */
 bool daObjHtetu1_c::_delete() {
-    /* Nonmatching */
+    /* Nonmatching*/
 }
 
 /* 00000610-00000648       .text check_sw__13daObjHtetu1_cFv */
-void daObjHtetu1_c::check_sw() {
+BOOL daObjHtetu1_c::check_sw() {
     /* Nonmatching */
+    return g_dComIfG_gameInfo.save.isSwitch(field_0x29C, home.roomNo);
 }
 
 /* 00000648-000006E4       .text init_mtx__13daObjHtetu1_cFv */
 void daObjHtetu1_c::init_mtx() {
     /* Nonmatching */
+    mpModel->setBaseScale(scale);
+    mDoMtx_stack_c::transS(current.pos);
+    mDoMtx_stack_c::ZXYrotM(shape_angle);
+    mpModel->setBaseTRMtx(mDoMtx_stack_c::get());
+    mpModel->calc();
 }
 
 /* 000006E4-000007F8       .text unlock__13daObjHtetu1_cFv */
@@ -64,6 +134,10 @@ bool daObjHtetu1_c::_execute() {
 /* 0000101C-0000107C       .text _draw__13daObjHtetu1_cFv */
 bool daObjHtetu1_c::_draw() {
     /* Nonmatching */
+    g_env_light.settingTevStruct(1, &current.pos, &tevStr);
+    g_env_light.setLightTevColorType(mpModel, &tevStr);
+    mDoExt_modelUpdateDL(mpModel);
+    return 1;
 }
 
 namespace {
