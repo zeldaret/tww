6ea6dd86575edb5f30e64b61fa960754cedb2d0a
TakaRikka <takarikka@outlook.com>
2026-09-20 22:34:25 -0700

    d_menu_item mostly done

--- include/d/d_menu_item.h
+++ include/d/d_menu_item.h
@@ -1,29 +1,34 @@
 #ifndef D_MENU_ITEM_H
 #define D_MENU_ITEM_H
 
-#include "dolphin/types.h"
 #include "d/d_menu_base.h"
+#include "f_op/f_op_msg_mng.h"
 #include "m_Do/m_Do_hostIO.h"
+#include "JSystem/J2DGraph/J2DWindow.h"
 
 struct fopMsgM_pane_class;
 class JKRArchive;
 class JUTFont;
 class J2DPane;
+class STControl;
+class dDlst_2DOutFont_c;
+class dMenu_save_c;
+class JPABaseEmitter;
 
 class dMenu_Item_c : public dMenu_base_c {
 public:
     void alphaChange(fopMsgM_pane_class*, f32) {}
-    virtual void draw() {}
-    u8 getItemMode() { return mItemMode; }
-    u8 getNowItem() { return mNowItem; }
-    void setNowItem(u8 val) { mNowItem = val; }
-    void setArchive(JKRArchive* arc) { mpArc = arc; }
-    void setFont(JUTFont* font, JUTFont* rfont) {
-        mFont = font;
-        mRFont = rfont;
+    virtual void draw() { _draw(); }
+    u8 getItemMode() { return itemMode; }
+    u8 getNowItem() { return nowItem; }
+    void setNowItem(u8 val) { nowItem = val; }
+    void setArchive(JKRArchive* arc) { archive = arc; }
+    void setFont(JUTFont* i_font, JUTFont* i_rfont) {
+        font[0] = i_font;
+        font[1] = i_rfont;
     }
-    void setItemTexBuffer(int idx, void* buffer) { mItemTexBuffer[idx] = buffer; }
-    void setSubItemTexBuffer(int idx, void* buffer) { mSubItemTexBuffer[idx] = buffer; }
+    void setItemTexBuffer(int idx, void* buffer) { itemTexBuffer[idx] = (ResTIMG*)buffer; }
+    void setSubItemTexBuffer(int idx, void* buffer) { subItemTexBuffer[idx] = (ResTIMG*)buffer; }
     void setTextArea(char* name0, char* name1, char* note0, char* note1, char* dummy0, char* dummy1) {
         name[0] = name0;
         name[1] = name1;
@@ -32,8 +37,8 @@ public:
         dummy[0] = dummy0;
         dummy[1] = dummy1;
     }
-    void setTimer(s16 val) { mTimer = val; }
-    void setTriggerInfo(u8 info) { mTriggerInfo = info; }
+    void setTimer(s16 val) { timer = val; }
+    void setTriggerInfo(u8 info) { triggerInfo = info; }
 
     void initialize();
     void screenSet();
@@ -41,7 +46,7 @@ public:
     void cursorMainMove();
     void cursorSubMove();
     void checkMove();
-    void itemplaceCheck(int);
+    bool itemplaceCheck(int);
     void itemDecide();
     void itemMove();
     void itemScale();
@@ -59,24 +64,24 @@ public:
     void noteAppear();
     void noteOpen();
     void noteClose();
-    void mainTrans(float, float);
-    void titleTrans(float, float);
-    void noteRotate(float, float);
-    void nameTrans(float, float);
-    void mainOpenProc(short, short, short);
-    void titleOpenProc(short, short);
-    void noteOpenProc(short, short);
-    void nameOpenProc(short, short);
-    void numberSet(J2DPane*, unsigned char);
-    void numberColor(J2DPane*, unsigned char);
+    void mainTrans(f32, f32);
+    void titleTrans(f32, f32);
+    void noteRotate(f32, f32);
+    void nameTrans(f32, f32);
+    void mainOpenProc(s16, s16, s16);
+    void titleOpenProc(s16, s16);
+    void noteOpenProc(s16, s16);
+    void nameOpenProc(s16, s16);
+    void numberSet(J2DPane*, u8);
+    void numberColor(J2DPane*, u8);
     void itemNumberSet();
     void itemCheck(int);
     void itemBitCheck(bool);
     void arrowLightAnime();
-    void bottleFwaterCheck();
-    void recollectBossCheck();
+    int bottleFwaterCheck();
+    BOOL recollectBossCheck();
     void cornerMove();
-    void equipBeastItem(int);
+    int equipBeastItem(int);
     virtual void _create();
     virtual void _delete();
     virtual void _move();
@@ -87,34 +92,94 @@ public:
     bool _close2();
 
 private:
-    /* 0x0004 */ u8 field_0x0004[0x2310 - 0x4];
-    /* 0x2310 */ JKRArchive* mpArc;
-    /* 0x2314 */ JUTFont* mFont;
-    /* 0x2318 */ JUTFont* mRFont;
-    /* 0x231C */ u8 padding_0x231C[0x2320 - 0x231C];
-    /* 0x2320 */ JUtility::TColor color_0x2320;
-    /* 0x2324 */ JUtility::TColor color_0x2324;
-    /* 0x2328 */ JUtility::TColor color_0x2328;
-    /* 0x232C */ JUtility::TColor color_0x232C;
-    /* 0x2330 */ u8 field_0x2330[0x2334 - 0x2330];
-    /* 0x2334 */ void* mItemTexBuffer[21];
-    /* 0x2388 */ u8 padding_0x2388[0x2394 - 0x2388];
-    /* 0x2394 */ void* mSubItemTexBuffer[9];
-    /* 0x23B8 */ u8 padding_0x23B8[0x23E0 - 0x23B8];
+    /* 0x0004 */ J2DScreen* scrn;
+    /* 0x0008 */ fopMsgM_pane_class field_0x8;
+    /* 0x0040 */ fopMsgM_pane_class field_0x40;
+    /* 0x0078 */ fopMsgM_pane_class field_0x78;
+    /* 0x00B0 */ fopMsgM_pane_class field_0xb0[15];
+    /* 0x03F8 */ fopMsgM_pane_class field_0x3f8[15];
+    /* 0x0740 */ fopMsgM_pane_class field_0x740;
+    /* 0x0778 */ fopMsgM_pane_class field_0x778;
+    /* 0x07B0 */ fopMsgM_pane_class field_0x7b0;
+    /* 0x07E8 */ fopMsgM_pane_class field_0x7e8;
+    /* 0x0820 */ fopMsgM_pane_class field_0x820;
+    /* 0x0858 */ fopMsgM_pane_class field_0x858;
+    /* 0x0890 */ fopMsgM_pane_class field_0x890[2];
+    /* 0x0900 */ fopMsgM_pane_class field_0x900;
+    /* 0x0938 */ fopMsgM_pane_class field_0x938;
+    /* 0x0970 */ fopMsgM_pane_class field_0x970;
+    /* 0x09A8 */ fopMsgM_pane_class field_0x9a8;
+    /* 0x09E0 */ fopMsgM_pane_class field_0x9e0;
+    /* 0x0A18 */ fopMsgM_pane_class field_0xa18[4];
+    /* 0x0AF8 */ fopMsgM_pane_class field_0xaf8[8];
+    /* 0x0CB8 */ fopMsgM_pane_class field_0xcb8[8];
+    /* 0x0E78 */ fopMsgM_pane_class field_0xe78[8];
+    /* 0x1038 */ fopMsgM_pane_class field_0x1038;
+    /* 0x1070 */ fopMsgM_pane_class field_0x1070[8];
+    /* 0x1230 */ fopMsgM_pane_class field_0x1230;
+    /* 0x1268 */ fopMsgM_pane_class field_0x1268[9];
+    /* 0x1460 */ fopMsgM_pane_class field_0x1460;
+    /* 0x1498 */ fopMsgM_pane_class field_0x1498;
+    /* 0x14D0 */ fopMsgM_pane_class field_0x14d0;
+    /* 0x1508 */ fopMsgM_pane_class field_0x1508;
+    /* 0x1540 */ fopMsgM_pane_class field_0x1540;
+    /* 0x1578 */ fopMsgM_pane_class field_0x1578;
+    /* 0x15B0 */ fopMsgM_pane_class field_0x15b0;
+    /* 0x15E8 */ fopMsgM_pane_class field_0x15e8;
+    /* 0x1620 */ fopMsgM_pane_class field_0x1620;
+    /* 0x1658 */ fopMsgM_pane_class field_0x1658[21];
+    /* 0x1AF0 */ fopMsgM_pane_class field_0x1af0[21];
+    /* 0x1F88 */ fopMsgM_pane_class field_0x1f88[3];
+    /* 0x2030 */ fopMsgM_pane_class field_0x2030[3];
+    /* 0x20D8 */ fopMsgM_pane_class field_0x20d8[6];
+    /* 0x2228 */ fopMsgM_pane_class field_0x2228;
+    /* 0x2260 */ fopMsgM_pane_class field_0x2260;
+    /* 0x2298 */ fopMsgM_pane_class field_0x2298;
+    /* 0x22D0 */ fopMsgM_pane_class field_0x22d0;
+    /* 0x2308 */ STControl* stick;
+    /* 0x230C */ dDlst_2DOutFont_c* outFont;
+    /* 0x2310 */ JKRArchive* archive;
+    /* 0x2314 */ JUTFont* font[2];
+    /* 0x231C */ J2DPane* field_0x231c;
+    /* 0x2320 */ J2DWindow::TContentsColor field_0x2320;
+    /* 0x2330 */ dMenu_save_c* dMs_c;
+    /* 0x2334 */ ResTIMG* itemTexBuffer[21];
+    /* 0x2388 */ u8 field_0x2388[0x2394 - 0x2388];
+    /* 0x2394 */ ResTIMG* subItemTexBuffer[8];
+    /* 0x23B4 */ ResTIMG* field_0x23b4;
+#if VERSION == VERSION_PAL
+    /* 0x23B8 */ ResTIMG* field_0x23b8_pal;
+    /* 0x23BC */ ResTIMG* field_0x23bc_pal;
+#endif
+    /* 0x23B8 */ JPABaseEmitter* field_0x23b8[2];
+    /* 0x23C0 */ f32 field_0x23c0[4];
+    /* 0x23D0 */ f32 field_0x23d0[4];
     /* 0x23E0 */ char* name[2];
     /* 0x23E8 */ char* note[2];
     /* 0x23F0 */ char* dummy[2];
-    /* 0x23F8 */ u16 mTimer;
-    /* 0x23FA */ u8 field_0x23FA[0x23FE - 0x23FA];
-    /* 0x23FE */ u8 mItemMode;
-    /* 0x23FF */ u8 mNowItem;
-    /* 0x2400 */ u8 padding_0x2400[0x2421 - 0x2400];
-    /* 0x2421 */ u8 mTriggerInfo;
+    /* 0x23F8 */ s16 timer;
+    /* 0x23FA */ s16 field_0x23fa;
+    /* 0x23FC */ s16 field_0x23fc;
+    /* 0x23FE */ u8 itemMode;
+    /* 0x23FF */ u8 nowItem;
+    /* 0x2400 */ u8 field_0x2400;
+    /* 0x2401 */ u8 field_0x2401;
+    /* 0x2402 */ u8 field_0x2402;
+    /* 0x2403 */ u8 field_0x2403;
+    /* 0x2404 */ u8 field_0x2404;
+    /* 0x2405 */ u8 field_0x2405;
+    /* 0x2406 */ u8 field_0x2406[21];
+    /* 0x241B */ u8 field_0x241B[0x241E - 0x241B];
+    /* 0x241E */ u8 field_0x241e;
+    /* 0x241F */ u8 field_0x241f;
+    /* 0x2420 */ u8 field_0x2420;
+    /* 0x2421 */ u8 triggerInfo;
     /* 0x2422 */ u8 field_0x2422;
-    /* 0x2423 */ u8 field_0x2423;
 }; // Size: 0x2424
 
+#if VERSION < VERSION_PAL
 STATIC_ASSERT(sizeof(dMenu_Item_c) == 0x2424);
+#endif
 
 class dMi_HIO_c : public JORReflexible {
 public:
