19d20d532551a1fae17725dccfb9e23c902287e0
Kenz Linden <mlinden1403@gmail.com>
2026-09-23 12:54:16 +1000

    d_a_obj_msdan_sub2 Delete and Mthd_Delete matching

--- src/d/actor/d_a_obj_msdan_sub2.cpp
+++ src/d/actor/d_a_obj_msdan_sub2.cpp
@@ -48,12 +48,15 @@ cPhs_State daObjMsdanSub2::Act_c::Mthd_Create() {
 
 /* 00000454-0000045C       .text Delete__Q214daObjMsdanSub25Act_cFv */
 BOOL daObjMsdanSub2::Act_c::Delete() {
-    /* Nonmatching */
+    return TRUE;
 }
 
 /* 0000045C-000004A8       .text Mthd_Delete__Q214daObjMsdanSub25Act_cFv */
 BOOL daObjMsdanSub2::Act_c::Mthd_Delete() {
-    /* Nonmatching */
+    BOOL flag = MoveBGDelete();
+    dComIfG_resDelete(&mPhs, M_arcname);
+
+    return flag;
 }
 
 /* 000004A8-00000528       .text set_mtx__Q214daObjMsdanSub25Act_cFv */
