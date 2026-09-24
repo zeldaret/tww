b57c63423fbae77da411a9155275d341bebbcf44
SuperDude88 <82904174+SuperDude88@users.noreply.github.com>
2026-09-23 22:10:04 -0400

    Enum Usage

--- src/d/actor/d_a_npc_rsh1.cpp
+++ src/d/actor/d_a_npc_rsh1.cpp
@@ -363,8 +363,8 @@ void daNpc_Rsh1_c::playTexPatternAnm() {
 /* 00000BDC-00000C64       .text setAnm__12daNpc_Rsh1_cFSc */
 void daNpc_Rsh1_c::setAnm(s8 i_index) {
     static int play_mode_tbl[7] = {
-        2, 2, 2, 2,
-        2, 2, 2
+        J3DFrameCtrl::EMode_LOOP, J3DFrameCtrl::EMode_LOOP, J3DFrameCtrl::EMode_LOOP, J3DFrameCtrl::EMode_LOOP,
+        J3DFrameCtrl::EMode_LOOP, J3DFrameCtrl::EMode_LOOP, J3DFrameCtrl::EMode_LOOP
     };
 
     static float morf_frame_tbl[7] = {
--- src/d/actor/d_a_tag_hint.cpp
+++ src/d/actor/d_a_tag_hint.cpp
@@ -479,7 +479,7 @@ void daTag_Hint_c::startProc() {
 
 /* 000018A4-000018AC       .text next_msgStatus__12daTag_Hint_cFPUl */
 u16 daTag_Hint_c::next_msgStatus(u32*) {
-    return 16;
+    return fopMsgStts_MSG_ENDS_e;
 }
 
 /* 000018AC-000018B4       .text getMsg__12daTag_Hint_cFv */
